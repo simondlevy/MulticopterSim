b2a77007545e1bddd66a1ecd744fddc60e18d93c
simondlevy <simon.d.levy@gmail.com>
2018-04-10 17:46:55 -0400

    Added Vision HUD

---
+++ Source/HackflightSim/VisionHUD.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "VisionHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HACKFLIGHTSIM_API AVisionHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
