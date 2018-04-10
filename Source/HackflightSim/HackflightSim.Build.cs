df1c033ec516f6b718fe5b45bd448a68ddfb91f1
simondlevy <simon.d.levy@gmail.com>
2018-04-10 17:36:20 -0400

    =

--- Source/HackflightSim/HackflightSim.Build.cs
+++ Source/HackflightSim/HackflightSim.Build.cs
@@ -1,6 +1,7 @@
 // Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using System;
 
 public class HackflightSim : ModuleRules
 {
@@ -9,5 +10,9 @@ public class HackflightSim : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+        // Un-comment and edit one of these lines to point to your Hackflight/src library
+        //PrivateIncludePaths.Add("/home/slevy/Documents/Arduino/libraries/Hackflight/src");         // Linux
+        PrivateIncludePaths.Add(Environment.GetEnvironmentVariable("userprofile") + "\\Documents\\Arduino\\libraries\\Hackflight\\src"); // Windows
 	}
 }
