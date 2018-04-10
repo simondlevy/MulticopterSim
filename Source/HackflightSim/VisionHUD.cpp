3bc934a4ba5af7998559f13be41592c606dfbdf8
simondlevy <simon.d.levy@gmail.com>
2018-04-10 18:16:31 -0400

    Rotate ground camera to follow vehicle

--- Source/HackflightSim/VisionHUD.cpp
+++ Source/HackflightSim/VisionHUD.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+e// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "VisionHUD.h"
 
