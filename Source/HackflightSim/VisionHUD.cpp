1ff7db604291be003621b65fe905514934b13bdd
simondlevy <simon.d.levy@gmail.com>
2018-04-10 22:51:00 -0400

    VisionHUD compiles

--- Source/HackflightSim/VisionHUD.cpp
+++ Source/HackflightSim/VisionHUD.cpp
@@ -2,6 +2,77 @@
 
 #include "VisionHUD.h"
 
+AVisionHUD::AVisionHUD()
+{
+	// Get Vision render target from blueprint
+	static ConstructorHelpers::FObjectFinder<UTextureRenderTarget2D> VisionTexObj(TEXT("/Game/Flying/RenderTargets/VisionHUDTarget"));
+	VisionTextureRenderTarget = VisionTexObj.Object;
+
+	// Creates Texture2D to store VisionTex content
+	UTexture2D* VisionTexture = UTexture2D::CreateTransient(VisionTextureRenderTarget->SizeX, VisionTextureRenderTarget->SizeY, PF_B8G8R8A8);
+
+#if WITH_EDITORONLY_DATA
+	VisionTexture->MipGenSettings = TMGS_NoMipmaps;
+#endif
+	VisionTexture->SRGB = VisionTextureRenderTarget->SRGB;
+
+	VisionRenderTarget = VisionTextureRenderTarget->GameThread_GetRenderTargetResource();
+
+	// Allocate memory for RGB image bytes
+	rows = VisionTextureRenderTarget->SizeY;
+	cols = VisionTextureRenderTarget->SizeX;
+	imagergb = new uint8_t[rows*cols * 3];
+}
+
+
+void AVisionHUD::DrawHUD()
+{
+	Super::DrawHUD();
+
+	// Draw the image to the HUD
+	DrawTextureSimple(VisionTextureRenderTarget, LEFTX, TOPY, 1.0f, true);
+
+	// Read the pixels from the RenderTarget and store them in a FColor array
+	VisionRenderTarget->ReadPixels(VisionSurfData);
+
+	// Convert the FColor array to an RGB byte array
+
+	for (int x = 0; x < cols; ++x) {
+
+		for (int y = 0; y < rows; ++y) {
+
+			int k = x + y * cols;
+
+			FColor PixelColor = VisionSurfData[k];
+
+			imagergb[k * 3] = PixelColor.R;
+			imagergb[k * 3 + 1] = PixelColor.G;
+			imagergb[k * 3 + 2] = PixelColor.B;
+		}
+	}
+
+	// Draw a border around the image
+
+	float rightx = LEFTX + WIDTH;
+	float bottomy = TOPY + HEIGHT;
+
+	drawBorder(LEFTX, TOPY, rightx, TOPY);
+	drawBorder(rightx, TOPY, rightx, bottomy);
+	drawBorder(rightx, bottomy, LEFTX, bottomy);
+	drawBorder(LEFTX, bottomy, LEFTX, TOPY);
+}
+
+void AVisionHUD::drawBorder(float lx, float uy, float rx, float by)
+{
+	DrawLine(lx, uy, rx, by, BORDER_COLOR, BORDER_WIDTH);
+}
+
+
+
+
+
+
+
 
 
 
--- Source/HackflightSim/VisionHUD.h
+++ Source/HackflightSim/VisionHUD.h
@@ -3,7 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine.h"
 #include "GameFramework/HUD.h"
+#include "GameFramework/HUD.h"
+#include "Engine/TextureRenderTarget2D.h"
+
 #include "VisionHUD.generated.h"
 
 /**
@@ -14,7 +18,27 @@ class HACKFLIGHTSIM_API AVisionHUD : public AHUD
 {
 	GENERATED_BODY()
 	
+	AVisionHUD();
+
+	virtual void DrawHUD() override;
+
+	const float LEFTX  = 20;
+	const float TOPY   = 20;
+	const float WIDTH  = 256;
+	const float HEIGHT = 128;
 	
-	
-	
+	const FLinearColor BORDER_COLOR = FLinearColor::Yellow;
+	const float BORDER_WIDTH = 2.0f;
+
+	void drawBorder(float lx, float uy, float rx, float by);
+
+	// Access to Vision camera
+	UTextureRenderTarget2D* VisionTextureRenderTarget;
+	FRenderTarget* VisionRenderTarget;
+	TArray<FColor> VisionSurfData;
+
+	// Support for vision algorithms
+	int rows;
+	int cols;
+	uint8_t* imagergb;
 };
