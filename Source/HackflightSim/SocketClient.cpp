426915659a61f46c8906248086b2d40147939abd
simondlevy <simon.d.levy@gmail.com>
2018-06-11 15:24:52 -0400

    Begin client version

---
+++ Source/HackflightSim/SocketClient.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SocketClient.h"
+
+SocketClient::SocketClient()
+{
+}
+
+SocketClient::~SocketClient()
+{
+}
