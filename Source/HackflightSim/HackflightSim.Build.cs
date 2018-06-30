c0f34b45bc2e56adb684c909296e9293bb79438c
simondlevy <simon.d.levy@gmail.com>
2018-06-29 20:05:08 -0400

    Added OpenCV support

--- Source/HackflightSim/HackflightSim.Build.cs
+++ Source/HackflightSim/HackflightSim.Build.cs
@@ -2,6 +2,7 @@
 
 using UnrealBuildTool;
 using System;
+using System.IO;
 
 public class HackflightSim : ModuleRules
 {
@@ -14,5 +15,54 @@ public class HackflightSim : ModuleRules
         // Un-comment and edit one of these lines to point to your Hackflight/src library
         //PrivateIncludePaths.Add("/home/slevy/Documents/Arduino/libraries/Hackflight/src");         // Linux
         PrivateIncludePaths.Add(Environment.GetEnvironmentVariable("userprofile") + "\\Documents\\Arduino\\libraries\\Hackflight\\src"); // Windows
-	}
+        LoadOpenCV(Target);
+    }
+
+    private string ThirdPartyPath
+    {
+        get { return Path.GetFullPath(Path.Combine(ModuleDirectory, "../../ThirdParty/")); }
+    }
+
+    public bool LoadOpenCV(ReadOnlyTargetRules Target)
+    {
+        // Start OpenCV linking here!
+        bool isLibrarySupported = false;
+
+        // Create OpenCV Path 
+        string OpenCVPath = Path.Combine(ThirdPartyPath, "OpenCV");
+
+        // Get Library Path 
+        string LibPath = "";
+        bool isdebug = Target.Configuration == UnrealTargetConfiguration.Debug && BuildConfiguration.bDebugBuildsActuallyUseDebugCRT;
+        if (Target.Platform == UnrealTargetPlatform.Win64)
+        {
+            LibPath = Path.Combine(OpenCVPath, "Libraries", "Win64");
+            isLibrarySupported = true;
+        }
+        else
+        {
+            string Err = string.Format("{0} dedicated server is made to depend on {1}. We want to avoid this, please correct module dependencies.", Target.Platform.ToString(), this.ToString());
+            System.Console.WriteLine(Err);
+        }
+
+        if (isLibrarySupported)
+        {
+            //Add Include path 
+            PublicIncludePaths.AddRange(new string[] { Path.Combine(OpenCVPath, "Includes") });
+
+            // Add Library Path 
+            PublicLibraryPaths.Add(LibPath);
+
+            //Add Static Libraries
+            PublicAdditionalLibraries.Add("opencv_world340.lib");
+
+            //Add Dynamic Libraries
+            PublicDelayLoadDLLs.Add("opencv_world340.dll");
+            PublicDelayLoadDLLs.Add("opencv_ffmpeg340_64.dll");
+        }
+
+        Definitions.Add(string.Format("WITH_OPENCV_BINDING={0}", isLibrarySupported ? 1 : 0));
+
+        return isLibrarySupported;
+    }
 }
