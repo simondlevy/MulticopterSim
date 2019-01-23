3ae636e773cf12377a4b219661b9a179e5a6af06
simondlevy <simon.d.levy@gmail.com>
2019-01-23 12:26:15 +0200

    More Receiver class factoring

--- Source/HackflightSim/HackflightSimReceiver.cpp
+++ Source/HackflightSim/HackflightSimReceiver.cpp
@@ -6,4 +6,76 @@
 * MIT License
 */
 
+#include "HackflightSimReceiver.h"
+
+hf::SimReceiver::SimReceiver(void) 
+{
+    _reversedVerticals = false;
+    _springyThrottle = false;
+    _useButtonForAux = false;
+    _joyid = 0;
+    _cycle = 0;
+
+    _buttonState = 0;
+}
+
+void hf::SimReceiver::begin(void)
+{
+    // Set up axes based on OS and controller
+    productInit();
+}
+
+bool hf::SimReceiver::gotNewFrame(void)
+{
+    return (++_cycle % 3) ? false : true;
+}
+
+void hf::SimReceiver::readRawvals(void)
+{
+    static int32_t axes[6];
+    static uint8_t buttons;
+
+    // Grab the axis values in an OS-specific way
+    productPoll(axes, buttons);
+
+    // Display axes (helps debug new controllers)
+    //hf::Debug::printf("0:%d  1:%d  2:%d 3:%d  4:%d  5:%d", axes[0], axes[1], axes[2], axes[3], axes[4], axes[5]);
+
+    // Normalize the axes to demands in [-1,+1]
+    for (uint8_t k=0; k<5; ++k) {
+        rawvals[k] = (axes[_axismap[k]] - productGetBaseline()) / 32767.f;
+    }
+
+    // Invert throttle, pitch if indicated
+    if (_reversedVerticals) {
+        rawvals[0] = -rawvals[0];
+        rawvals[2] = -rawvals[2];
+    }
+
+    // For game controllers, use buttons to fake up values in a three-position aux switch
+    if (_useButtonForAux) {
+        for (uint8_t k=0; k<3; ++k) {
+            if (buttons == _buttonmap[k]) {
+                _buttonState = k;
+            }
+        }
+        rawvals[4] = buttonsToAux[_buttonState];
+    }
+}
+
+void hf::SimReceiver::halt(void)
+{
+}
+
+
+uint8_t hf::SimReceiver::getAux1State(void) 
+{
+    return _springyThrottle ? 2 : Receiver::getAux1State();
+}
+
+uint8_t hf::SimReceiver::getAux2State(void)
+{
+    return 1; // always armed!
+}
+
 
--- Source/HackflightSim/HackflightSimReceiver.h
+++ Source/HackflightSim/HackflightSimReceiver.h
@@ -22,76 +22,21 @@ namespace hf {
 
         public:
 
-            SimReceiver(void) 
-            {
-                _reversedVerticals = false;
-                _springyThrottle = false;
-                _useButtonForAux = false;
-                _joyid = 0;
-                _cycle = 0;
+            SimReceiver(void);
 
-                _buttonState = 0;
-            }
+            void begin(void);
 
-            void begin(void)
-            {
-                // Set up axes based on OS and controller
-                productInit();
-            }
+            bool gotNewFrame(void);
 
-            bool gotNewFrame(void)
-            {
-                return (++_cycle % 3) ? false : true;
-            }
+            void readRawvals(void);
 
-            void readRawvals(void)
-            {
-                static int32_t axes[6];
-                static uint8_t buttons;
-
-                // Grab the axis values in an OS-specific way
-                productPoll(axes, buttons);
-
-                // Display axes (helps debug new controllers)
-                //hf::Debug::printf("0:%d  1:%d  2:%d 3:%d  4:%d  5:%d", axes[0], axes[1], axes[2], axes[3], axes[4], axes[5]);
-
-                // Normalize the axes to demands in [-1,+1]
-                for (uint8_t k=0; k<5; ++k) {
-                    rawvals[k] = (axes[_axismap[k]] - productGetBaseline()) / 32767.f;
-                }
-
-                // Invert throttle, pitch if indicated
-                if (_reversedVerticals) {
-                    rawvals[0] = -rawvals[0];
-                    rawvals[2] = -rawvals[2];
-                }
-
-                // For game controllers, use buttons to fake up values in a three-position aux switch
-                if (_useButtonForAux) {
-                    for (uint8_t k=0; k<3; ++k) {
-                        if (buttons == _buttonmap[k]) {
-                            _buttonState = k;
-                        }
-                    }
-                    rawvals[4] = buttonsToAux[_buttonState];
-                }
-            }
-
-            void halt(void)
-            {
-            }
+            void halt(void);
 
         protected:
 
-            virtual uint8_t getAux1State(void) override
-            {
-                return _springyThrottle ? 2 : Receiver::getAux1State();
-            }
+            virtual uint8_t getAux1State(void) override;
 
-            virtual uint8_t getAux2State(void) override
-            {
-                return 1; // always armed!
-            }
+            virtual uint8_t getAux2State(void) override;
 
             virtual void productInit(void) = 0;
             virtual void productPoll(int32_t axes[6], uint8_t & buttons) = 0;
