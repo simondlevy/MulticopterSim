33ad7db49a9cafb3bb5ce6a29143d1da914a3921
simondlevy <simon.d.levy@gmail.com>
2019-01-25 10:36:59 +0200

    Begin Joystick class

---
+++ Source/MulticopterSim/Joystick.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Joystick.h"
+
+#include "VehiclePawn.h"
+
+#include <shlwapi.h>
+#include "joystickapi.h"
+
+
+
+Joystick::Joystick()
+{
+}
+
+Joystick::~Joystick()
+{
+}
