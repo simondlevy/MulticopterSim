00e48d4b2a8d99f363ce02d45e008e87961f5a03
unknown <simondlevy>
2019-02-14 17:28:10 +0200

    Cleanup

--- Source/MulticopterSim/VehiclePawn.cpp
+++ Source/MulticopterSim/VehiclePawn.cpp
@@ -60,10 +60,10 @@ AVehiclePawn::AVehiclePawn()
 	_propellerAudioComponent->SetupAttachment(GetRootComponent());
 
     // Set up the FPV camera
-    _fpvSpringArm = CreateDefaultSubobject<USpringArmComponent>(L"fpvSpringArm");
+    _fpvSpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("fpvSpringArm"));
     _fpvSpringArm->SetupAttachment(RootComponent);
     _fpvSpringArm->TargetArmLength = 0.f; // The camera follows at this distance behind the character
-    _fpvCamera = CreateDefaultSubobject<UCameraComponent>(L"fpvCamera");
+    _fpvCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("fpvCamera"));
     _fpvCamera ->SetupAttachment(_fpvSpringArm, USpringArmComponent::SocketName); 
 }
 
