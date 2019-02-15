a904019475236fb04d1f373c6a9aaab2ed8c5548
simondlevy <simon.d.levy@gmail.com>
2019-02-15 17:16:32 +0200

    More Linux migration

--- Source/MulticopterSim/hackflight/BuiltinPhysics.h
+++ Source/MulticopterSim/hackflight/BuiltinPhysics.h
@@ -1,15 +1,47 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/*
+ * BuiltinPhysics.h: Physics class using UE4 built-in physics 
+ *
+ * Copyright (C) 2019 Simon D. Levy
+ *
+ * MIT License
+ */
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "Physics.h"
+#include "VehiclePawn.h"
+//#include "Joystick.h"
+#include "FlightManager.h"
 
-/**
- * 
- */
-class MULTICOPTERSIM_API BuiltinPhysics
-{
-public:
-	BuiltinPhysics();
-	~BuiltinPhysics();
+// Math support
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+
+class MULTICOPTERSIM_API BuiltinPhysics : public Physics {
+
+    private:
+
+		// For simulating gyro
+		FVector _eulerPrev;
+
+		// Abstract class for flight control
+        FlightManager * _flightManager;
+
+        void computeForces(float deltaSeconds, TArray<float> motorValues, FVector & euler, FVector & rotationForce, FVector & translationForce);
+
+        static constexpr float THRUST_FACTOR = 130.f;
+
+        static float motorsToAngularForce(TArray<float> motorValues, uint8_t a, uint8_t b, uint8_t c, uint8_t d);
+
+        static float sum(TArray<float> x);
+
+ public:
+
+        BuiltinPhysics(class AVehiclePawn * vehiclePawn, class UStaticMeshComponent* vehicleMesh);
+
+        virtual void start(void) override;
+
+        virtual TArray<float> update(float deltaSeconds) override;
+
 };
