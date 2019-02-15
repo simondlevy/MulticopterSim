cdf33af2c60500f67a9fa1322908a6f58326fc9f
simondlevy <simon.d.levy@gmail.com>
2019-02-15 12:10:39 +0200

    =More Linux migration

--- Source/MulticopterSim/VehiclePawn.cpp
+++ Source/MulticopterSim/VehiclePawn.cpp
@@ -23,8 +23,6 @@
 
 AVehiclePawn::AVehiclePawn()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
 
 }
 
