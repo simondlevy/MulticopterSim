d8a724300b15ce9c7805b2af7e0b1a2271a3f759
simondlevy <simon.d.levy@gmail.com>
2019-02-16 00:02:35 +0200

    =More Linux migration

--- Source/MulticopterSim/hackflight/SimBoard.h
+++ Source/MulticopterSim/hackflight/SimBoard.h
@@ -11,14 +11,32 @@
 
 #include <board.hpp>
 
-
 #pragma once
 
-class SimBoard {
+class SimBoard : public hf::Board {
 
-public:
+    private:
+
+        float _currentTime = 0;
+		FQuat _quat = FQuat(0,0,0,0);
+		FVector _gyro = FVector(0, 0, 0);
+		TArray<float> _motors = { 0,0,0,0 };
+
+    protected:
+
+        bool getQuaternion(float quat[4]) override;
+
+        bool getGyrometer(float gyro[3]) override;
+
+        float getTime(void) override;
+
+        void writeMotor(uint8_t index, float value) override;
+
+ public:
 
 	SimBoard();
 
 	~SimBoard();
+
+    TArray<float> update(float currentTime, FQuat quat, FVector gyro);
 };
