ca4525bd26e0b82181ac03d21ea922c2e439d992
simondlevy <simon.d.levy@gmail.com>
2019-02-15 12:04:25 +0200

    =More Linux migration

--- Source/MulticopterSim/VehiclePawn.cpp
+++ Source/MulticopterSim/VehiclePawn.cpp
@@ -7,9 +7,20 @@
 */
 
 #include "VehiclePawn.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Camera/CameraComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Engine.h"
+#include "Engine/World.h"
+#include "Engine/StaticMesh.h"
+#include "Runtime/Core/Public/Math/UnrealMathUtility.h"
 
+#include <cmath>
+#include <stdarg.h>
 
-// Sets default values
 AVehiclePawn::AVehiclePawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
