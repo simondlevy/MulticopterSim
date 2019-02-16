1cf2031cc1e31fdc3bbac4ac44bd1749450715c1
simondlevy <simon.d.levy@gmail.com>
2019-02-16 23:56:19 +0200

    More Linux migration

--- Source/MulticopterSim/JoystickLinux.cpp
+++ Source/MulticopterSim/JoystickLinux.cpp
@@ -15,33 +15,6 @@
 #include <sys/time.h>
 #include <linux/joystick.h>
 
-/*
-void Joystick::poll(float axes[6], uint8_t & buttonState)
-{
-    if (_joystickId <= 0) return;
-
-    struct js_event js;
-
-    read(_joystickId, &js, sizeof(struct js_event));
-
-    if (js.type & JS_EVENT_INIT) return;
-
-    switch (js.type) {
-
-        case JS_EVENT_AXIS:
-            switch (_productId) {
-                case PRODUCT_F310:// 1,3,4,0
-                    break;
-            }
-            axes[js.number] = js.value;
-            break;
-
-        case JS_EVENT_BUTTON:
-            _buttons = js.number + 1; // avoid zero
-    }
-}
-*/
-
 class LinuxJoystick : public Joystick {
 
     private:
@@ -50,11 +23,18 @@ class LinuxJoystick : public Joystick {
 
         int _joystickId;
 
+        uint8_t _axisMap[6];
+        uint8_t _buttonMap[3];
+        bool _reversedVerticals;
+        bool _useButtonForAux;
+
     public:
 
         LinuxJoystick(void) 
         {
             isRcTransmitter = false;
+            _reversedVerticals = false;
+            _useButtonForAux = false;
 
             _joystickId = open(DEVNAME, O_RDONLY);
 
@@ -69,22 +49,78 @@ class LinuxJoystick : public Joystick {
                 }
 
                 if (strstr(productName, "Taranis") || strstr(productName, "DeviationTx Deviation GamePad")) {
+                    _axisMap[0] = 0;
+                    _axisMap[1] = 1;
+                    _axisMap[2] = 2;
+                    _axisMap[3] = 3;
                     isRcTransmitter = true;
                 }
                 else if (strstr(productName, "Horizon Hobby SPEKTRUM")) {
+                    _axisMap[0] = 1;
+                    _axisMap[1] = 2;
+                    _axisMap[2] = 3;
+                    _axisMap[3] = 0;
+                    _axisMap[4] = 4;
                     isRcTransmitter = true;
                 }
                 else if (strstr(productName, "Extreme 3D")) {
+                    _axisMap[0] = 3;
+                    _axisMap[1] = 0;
+                    _axisMap[2] = 1;
+                    _axisMap[3] = 2;
+                    _reversedVerticals = true;
+                    _useButtonForAux = true;
+                    _buttonMap[0] = 1;
+                    _buttonMap[1] = 2;
+                    _buttonMap[2] = 3;
                 }
                 else if (strstr(productName, "Generic X-Box pad")) {
+                    _axisMap[0] =  1;
+                    _axisMap[1] =  3;
+                    _axisMap[2] =  4;
+                    _axisMap[3] =  0;
+                    _reversedVerticals = true;
+                    _useButtonForAux = true;
+                    _buttonMap[0] = 4;
+                    _buttonMap[1] = 2;
+                    _buttonMap[2] = 1;
                 }
                 else if (strstr(productName, "Logitech Gamepad F310")) {
+                }
+                else { // default to PS3
+                    _axisMap[0] = 1;
+                    _axisMap[1] = 3;
+                    _axisMap[2] = 4;
+                    _axisMap[3] = 0;
+                    _reversedVerticals = true;
+                    _useButtonForAux = true;
+                    _buttonMap[0] = 1;
+                    _buttonMap[1] = 2;
+                    _buttonMap[2] = 3;
                 }
             }
         }
 
         virtual void poll(float axes[6], uint8_t & buttonState) override
         {
+            if (_joystickId <= 0) return;
+
+            struct js_event js;
+
+            read(_joystickId, &js, sizeof(struct js_event));
+
+            if (js.type & JS_EVENT_INIT) return;
+
+            switch (js.type) {
+
+                case JS_EVENT_AXIS:
+                    //axes[js.number] = js.value;
+                    break;
+
+                case JS_EVENT_BUTTON:
+                    //_buttons = js.number + 1; // avoid zero
+                    break;
+            }
         }
 };
 
@@ -92,12 +128,5 @@ Joystick * Joystick::createJoystick(void)
 {
     return new LinuxJoystick();
 }
-
-/*
-   int32_t hf::Controller::productGetBaseline(void)
-   {
-   return 0;
-   }
- */
 
 #endif
