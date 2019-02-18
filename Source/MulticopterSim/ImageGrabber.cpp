4119190aa2e432543287c4e95007ea93b86c74b1
simondlevy <simon.d.levy@gmail.com>
2019-02-18 10:33:47 -0600

    Begin restoring image-grabber support

---
+++ Source/MulticopterSim/ImageGrabber.cpp
@@ -0,0 +1,48 @@
+/*
+ * ImageGrabber.cpp: MulticopterSim support for acquisition of camera images and processing
+ *
+ * Adapted from https://answers.unrealengine.com/questions/193827/how-to-get-texture-pixels-using-utexturerendertarg.html
+ *
+ * Copyright (C) 2019 Simon D. Levy
+ *
+ * MIT License
+ */
+
+#include "ImageGrabber.h"
+
+#include "opencv2/imgproc/imgproc.hpp"
+
+ImageGrabber::ImageGrabber(UTextureRenderTarget2D* visionTextureRenderTarget)
+{
+	// Get the size of the render target
+	uint16_t rows = visionTextureRenderTarget->SizeY;
+	uint16_t cols = visionTextureRenderTarget->SizeX;
+
+	// Create Texture2D to store render content
+	UTexture2D* texture = UTexture2D::CreateTransient(cols, rows, PF_B8G8R8A8);
+
+#if WITH_EDITORONLY_DATA
+	texture->MipGenSettings = TMGS_NoMipmaps;
+#endif
+
+	texture->SRGB = visionTextureRenderTarget->SRGB;
+
+	_renderTarget = visionTextureRenderTarget->GameThread_GetRenderTargetResource();
+}
+
+ImageGrabber::~ImageGrabber(void)
+{
+}
+
+// Runs on main thread
+void ImageGrabber::grabImage(void)
+{
+	// Read the pixels from the RenderTarget
+	TArray<FColor> SurfData;
+	_renderTarget->ReadPixels(SurfData);
+
+	// Copy the pixels to the OpenCV Mat data
+	//FMemory::Memcpy(_img.data, SurfData.GetData(), SurfData.Num() * 4);
+
+}
+
---
+++ Source/MulticopterSim/ImageGrabber.h
@@ -0,0 +1,38 @@
+/*
+ * ImageGrabber.h: MulticopterSim support for acquisition of camera images and processing
+ *
+ * You should subclass this class, implementing the processImage() method
+ *
+ * Copyright (C) 2019 Simon D. Levy
+ *
+ * MIT License
+ */
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine.h"
+#include "GameFramework/HUD.h"
+#include "GameFramework/HUD.h"
+#include "Engine/TextureRenderTarget2D.h"
+
+class ImageGrabber
+{
+
+public:
+
+	ImageGrabber(UTextureRenderTarget2D* visionTextureRenderTarget);
+
+	~ImageGrabber(void);
+
+	// Called on the main thread
+	void grabImage(void);
+
+	// Called on the other thread
+	virtual void processImage(void) { }
+
+private:
+
+	// Access to camera's render target
+	FRenderTarget* _renderTarget;
+};
