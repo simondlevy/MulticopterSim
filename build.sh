5e14b12c56bfdfad4c37d5b538f7b1ee0c88e8f2
simondlevy <simon.d.levy@gmail.com>
2019-02-18 13:54:25 +0200

    =Cleanup

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 # Change this to wherever you installed UnrealEngine
 UE=$HOME/UnrealEngine
 
-ENGINE=$UE/Engine
-
-$ENGINE/Build/BatchFiles/Linux/RunMono.sh  $ENGINE/Binaries/DotNET/UnrealBuildTool.exe Development Linux \
+$UE/Engine/Build/BatchFiles/Linux/RunMono.sh  $UE/Engine/Binaries/DotNET/UnrealBuildTool.exe Development Linux \
     -Project="/home/simon/Documents/Unreal Projects/MulticopterSim/MulticopterSim.uproject" \
     -TargetType=Editor -Progress -NoHotReloadFromIDE
