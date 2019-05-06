6e9774f7b616f5b5416973d18c3198816fd741b5
simondlevy <simon.d.levy@gmail.com>
2019-05-06 13:14:24 +0300

    FlightManager constructor gets VehiclePawn object

--- Source/MulticopterSim/FlightManager.h
+++ Source/MulticopterSim/FlightManager.h
@@ -1,22 +1,24 @@
 /*
-* FlightManager.h: Abstract flight-management class for MulticopterSim
-*
-* Copyright (C) 2019 Simon D. Levy
-*
-* MIT License
-*/
+ * FlightManager.h: Abstract flight-management class for MulticopterSim
+ *
+ * Copyright (C) 2019 Simon D. Levy
+ *
+ * MIT License
+ */
 
 #pragma once
 
+#include "VehiclePawn.h"
+
 class MULTICOPTERSIM_API FlightManager
 {
-public:
+    public:
 
-    virtual TArray<float> update(float currentTime, FQuat quat, FVector gyro)  = 0;
+        virtual TArray<float> update(float currentTime, FQuat quat, FVector gyro)  = 0;
 
-    /**
-     *  Factory method.
-     *  @return pointer to a new FlightManager object
-     */
-     static FlightManager * createFlightManager(void);
+        /**
+         *  Factory method.
+         *  @return pointer to a new FlightManager object
+         */
+        static FlightManager * createFlightManager(AVehiclePawn * vehiclePawn);
 };
