69c1811a82e8b9bc2a0612782da3cb2bdeb1f019
unknown <simondlevy>
2019-05-14 21:01:12 +0300

    Begin threaded dynamics

---
+++ Source/MulticopterSim/DynamicsWorker.cpp
@@ -0,0 +1,37 @@
+/*
+ * DynamicsWorker.cpp: ThreadedWorker subclass for dynamics computation
+ *
+ * This class contains very little code, because the dynamics computations are 
+ * done in the platform-independent MultirotorDynamics class.
+ *
+ * Copyright (C) 2019 Simon D. Levy
+ *
+ * MIT License
+ */
+
+#include "DynamicsWorker.h"
+
+// Called once on main thread
+FDynamicsWorker::FDynamicsWorker(AVehiclePawn * vehiclePawn, class MultirotorDynamics * dynamics) : FThreadedWorker(vehiclePawn)
+{
+    _dynamics = dynamics;
+
+    _previousTime = 0;
+}
+
+FDynamicsWorker::~FDynamicsWorker(void)
+{
+}
+
+// Called repeatedly on worker thread
+void FDynamicsWorker::performTask(void)
+{
+    double currentTime = getCurrentTime();
+
+    if (_previousTime>0) {
+
+        _dynamics->update(currentTime-_previousTime);
+    }
+
+    _previousTime = currentTime;
+}
---
+++ Source/MulticopterSim/DynamicsWorker.h
@@ -0,0 +1,32 @@
+/*
+ * DynamicsWorker.h: ThreadedWorker subclass for dynamics computation
+ *
+ * Copyright (C) 2019 Simon D. Levy
+ *
+ * MIT License
+ */
+
+#pragma once
+
+#include "ThreadedWorker.h"
+#include "dynamics/MultirotorDynamics.h"
+
+class FDynamicsWorker :	public FThreadedWorker {
+
+    private:
+
+        class MultirotorDynamics * _dynamics;
+
+        double _previousTime;
+
+
+    protected:
+
+        virtual void performTask(void) override;
+
+    public:
+
+        FDynamicsWorker(class AVehiclePawn * vehiclePawn, class MultirotorDynamics * dynamics);
+
+        ~FDynamicsWorker(void);
+};
