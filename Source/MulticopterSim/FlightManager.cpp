bd39ac403ec0e2d4645f1f635b529d986207891b
unknown <simondlevy>
2019-05-22 18:46:25 +0300

    Successfully computing vertical position

--- Source/MulticopterSim/FlightManager.cpp
+++ Source/MulticopterSim/FlightManager.cpp
@@ -67,7 +67,7 @@ void FFlightManager::performTask(void)
         _dynamics->update(deltaT);
         _newDynamics->update(deltaT);
 
-        dbgprintf("dZ/dt: %f", _newDynamics->_x[NewMultirotorDynamics::STATE_Z_DOT]);
+        dbgprintf("Z: %f", _newDynamics->_x[NewMultirotorDynamics::STATE_Z]);
 
         // Get vehicle state from dynamics.  We keep pose (position, rotation) in memory for use  in
         // getKinematics() method
