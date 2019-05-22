ed7c60a7d0415578b054eb09fd9137e9a1e4b073
simondlevy <simon.d.levy@gmail.com>
2019-05-22 15:16:38 +0300

    Begin migrating new dynamics into FlightManager

---
+++ Source/MulticopterSim/dynamics/NewMultirotorDynamics.hpp
@@ -0,0 +1,220 @@
+/*
+ * Header-only code for platform-independent multirotor dynamics
+ *
+ * Should work for any simulator, vehicle, or operating system
+ *
+ * Based on:
+ *
+ *   @inproceedings{DBLP:conf/icra/BouabdallahMS04,
+ *     author    = {Samir Bouabdallah and Pierpaolo Murrieri and Roland Siegwart},
+ *     title     = {Design and Control of an Indoor Micro Quadrotor},
+ *     booktitle = {Proceedings of the 2004 {IEEE} International Conference on Robotics
+ *                  and Automation, {ICRA} 2004, April 26 - May 1, 2004, New Orleans,
+ *                  LA, {USA}},
+ *     pages     = {4393--4398},
+ *     year      = {2004},
+ *     crossref  = {DBLP:conf/icra/2004},
+ *     url       = {https://doi.org/10.1109/ROBOT.2004.1302409},
+ *     doi       = {10.1109/ROBOT.2004.1302409},
+ *     timestamp = {Sun, 04 Jun 2017 01:00:00 +0200},
+ *     biburl    = {https://dblp.org/rec/bib/conf/icra/BouabdallahMS04},
+ *     bibsource = {dblp computer science bibliography, https://dblp.org}
+ *   }
+ * 
+ * Copyright (C) 2019 Simon D. Levy
+ *
+ * MIT License
+ */
+
+#pragma once
+
+class MultirotorDynamics {
+
+    //private:
+    public:
+
+        // Universal constants
+        static constexpr double g  = 9.80665; // might want to allow this to vary!
+        static constexpr double pi = 3.14159;
+
+        // State vector (see Eqn. 11)
+        double _x[12] = {0};
+
+        // Set by subclass constructor
+        int _nmotors;
+
+        // Values computed in Equation 6
+        double _U1 = 0;
+        double _U2 = 0;
+        double _U3 = 0;
+        double _U4 = 0;
+        double _Omega = 0;
+
+        // Radians per second for each motor
+        double * _omegas;
+
+        // Kinematics
+        double _location[3] = {0};
+        double _rotation[3] = {0};
+
+        // Flag for whether we're airborne
+        bool _airborne = false;
+
+    protected:
+
+        /** 
+         * You must implement these methods in a subclass for each vehicle.
+         */
+
+        virtual const double b(void)  = 0;
+        virtual const double d(void)  = 0;
+        virtual const double m(void)  = 0;
+        virtual const double l(void)  = 0;
+        virtual const double Ix(void) = 0;
+        virtual const double Iy(void) = 0;
+        virtual const double Iz(void) = 0; 
+        virtual const double Jr(void) = 0;
+        virtual const unsigned int maxrpm(void) = 0;
+
+        virtual double u2(double * o2)  = 0;
+        virtual double u3(double * o2)  = 0;
+        virtual double u4(double * o2)  = 0;
+
+        virtual double omega(double * o) = 0;
+
+        /**
+         *  Constructor
+         */
+        MultirotorDynamics(int nmotors)
+        {
+            _omegas = new double[nmotors];
+
+            while (true) ;
+        }
+
+    public:
+
+        /**
+         *  Destructor
+         */
+        virtual ~MultirotorDynamics(void)
+        {
+            delete _omegas;
+        }
+
+        /** 
+         * Initializes pose, with flag for whether we're airbone (helps with testing gravity).
+         *
+         * @param location X,Y,Z
+         * @param rotation phi, theta, psi
+         * @param airborne allows us to start on the ground (default) or in the air (e.g., gravity test)
+         */
+        void init(double location[3], double rotation[3], bool airborne=false)
+        {
+            // Zero-out entire state
+            for (int i=0; i<12; ++i) {
+                _x[i] = 0;
+             }
+
+            // Set kinematics
+            for (int i=0; i<3; ++i) {
+                _location[i] = _x[i*2]   = location[i];
+                _rotation[i] = _x[i*2+6] = rotation[i];
+            }
+ 
+            // We can start on the ground (default) or in the air
+            _airborne = airborne;
+        }
+
+        /** 
+         * Updates state.
+         *
+         * @param dt time in seconds since previous update
+         */
+        void update(double dt)
+        {
+            // Equation 12: replace state components with their first derivatives
+            _x[0]  = _x[1];
+            _x[1]  = (cos(_x[6])*sin(_x[8])*cos(_x[10]) + sin(_x[6])*sin(_x[10])) * _U1 / m();
+            _x[2]  = _x[3];
+            _x[3]  = (cos(_x[6])*sin(_x[8])*sin(_x[10]) + sin(_x[6])*cos(_x[10])) * _U1 / m();
+            _x[4]  = _x[5];
+            _x[5]  = -g + (cos(_x[6])*cos(_x[8])) * _U1 / m();
+            _x[6]  = _x[7];
+            _x[7]  = _x[11]*_x[9]*(Iy()-Iz())/Ix() - Jr()/Ix()*_x[9]*_Omega + l()/Ix()*_U2;
+            _x[8]  = _x[9];
+            _x[9]  = _x[11]*_x[7]*(Iz()-Ix())/Iy()   + Jr()/Iy()*_x[7]*_Omega   + l()/Iy()*_U3; 
+            _x[10] = _x[11];
+            _x[11] = _x[9]*_x[7]*(Ix()-Iy())/Iz() + l()/Iz()*_U4;
+
+            // Integrate first derivatives to get kinematics
+            for (int i=0; i<3; ++i) {
+                _location[i] += dt * _x[i*2];
+                _rotation[i] += dt * _x[i*2+6];
+            }
+        }
+
+        /**
+         * Uses motor values to implement Equation 6.
+         *
+         * @param motorvals in interval [0,1]
+         */
+        void setMotors(double * motorvals) 
+        {
+            // ====================
+            // For any vehicle, U1 is always the scaled sum of the motor omegas
+            _U1 = 0;
+
+            // Convert the  motor values to radians per second, accumulating U1
+            for (int i=0; i<_nmotors; ++i) {
+                //_omegas[i] = motorvals[i] * maxrpm() * pi / 30;
+                //_U1 += _omegas[i];
+            }
+
+            return;
+
+            // Scale U1
+            _U1 *= b();
+
+            // Compute Omega from Omegas
+            _Omega = omega(_omegas);
+
+            // Square the Omegas
+            for (int i=0; i<_nmotors; ++i) {
+                _omegas[i] *= _omegas[i];
+            }
+
+            // Use the squared Omegas to implement the rest of Eqn. 6
+            _U2 = b() * u2(_omegas);
+            _U3 = b() * u3(_omegas);
+            _U4 = d() * u4(_omegas);
+
+            // ====================
+        }
+
+        /*
+         *  Gets current state
+         *
+         *  @param angularVelocity
+         *  @param eulerAngles
+         *  @param velocityXYZ
+         *  @param locationXYZ
+         */
+        void getState(double angularVelocity[3], double eulerAngles[3], double velocityXYZ[3], double locationXYZ[3])
+        {
+        }
+
+        /**
+         * Factory method
+         */
+        static MultirotorDynamics * create(void);
+
+}; // class MultirotorDynamics
+
+
+
+
+
+
+
+
---
+++ Source/MulticopterSim/dynamics/NewQuadPlusDynamics.cpp
@@ -0,0 +1,70 @@
+/*
+ * MultirotorDynamics implementation for a plus-confuration quadcopter:
+ *
+ *         1cw
+ *          |
+ *          |
+ *   4ccw - + - 2ccw
+ *          |
+ *          |
+ *         3cw
+ *
+ * For reference citation see MultirotorDynamics.hpp
+ *
+ * Copyright (C) 2019 Simon D. Levy
+ *
+ * MIT License
+ */
+
+#include "MultirotorDynamics.hpp"
+
+class QuadPlusDynamics : public MultirotorDynamics {
+
+    protected:
+
+        // Constants
+        const double b(void)  override  { return 0.0000530216718361085; }
+        const double d(void)  override  { return 2.23656692806239E-06; }
+        const double m(void)  override  { return 16.47; }
+        const double l(void)  override  { return 0.6; }
+        const double Ix(void) override  { return 2; }
+        const double Iy(void) override  { return 2; }
+        const double Iz(void) override  { return 3; }
+        const double Jr(void) override  { return 0.000308013; } // Kg*m^2
+
+        const unsigned int maxrpm(void) override { return 10000; }
+
+        // Eqn. 6 -------------------------------------------
+        
+        virtual double u2(double * o2) override
+        {
+            return o2[3] - o2[1];
+        }
+
+        virtual double u3(double * o2) override
+        {
+            return o2[2] - o2[0];
+        }
+
+        virtual double u4(double * o2) override
+        {
+            return o2[1] + o2[3] - o2[0] - o2[2];
+        }
+
+        virtual double omega(double * o)
+        {
+            return o[1] + o[3] - o[0] - o[2];
+        }
+
+    public:
+
+        QuadPlusDynamics(void) : MultirotorDynamics(4) { }
+
+}; // class QuadPlusDynamics
+
+// Factory method
+MultirotorDynamics * MultirotorDynamics::create(void)
+{
+    return new QuadPlusDynamics();
+}
+
