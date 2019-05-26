1cad86d46f557b1f883f2fea4d3003a8daa0b390
simondlevy <simon.d.levy@gmail.com>
2019-05-26 12:40:48 +0300

    Added plotting program for debugging

---
+++ Source/MulticopterSim/dynamics/test/plotlog.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+LOGFILE = 'C:\\Users\sim\Desktop\log.csv'
+
+data = np.genfromtxt(LOGFILE, delimiter=',')
+
+t = data[:,0]
+
+plt.subplot(3,1,1)
+plt.plot(t,data[:,1])
+plt.ylabel("Z''")
+
+plt.subplot(3,1,2)
+plt.plot(t, data[:,2])
+plt.ylabel("Z'")
+
+plt.subplot(3,1,3)
+plt.plot(t, data[:,3])
+plt.ylabel("Z");
+
+plt.show()
