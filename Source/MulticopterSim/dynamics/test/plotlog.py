e6498ee2cf81b03f45f5f4d64e44480598811e77
simondlevy <simon.d.levy@gmail.com>
2019-05-26 12:55:32 +0300

    Invert Y axis

--- Source/MulticopterSim/dynamics/test/plotlog.py
+++ Source/MulticopterSim/dynamics/test/plotlog.py
@@ -14,9 +14,11 @@ plt.ylabel("Z''")
 plt.subplot(3,1,2)
 plt.plot(t, data[:,2])
 plt.ylabel("Z'")
+plt.ylim(plt.ylim()[::-1])
 
 plt.subplot(3,1,3)
 plt.plot(t, data[:,3])
 plt.ylabel("Z");
+plt.ylim(plt.ylim()[::-1])
 
 plt.show()
