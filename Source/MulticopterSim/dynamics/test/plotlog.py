3e36728800d7a1749ff50fdc2bec8f6bb75067ab
simondlevy <simon.d.levy@gmail.com>
2019-05-26 14:21:09 +0300

    Cleanup plot program

--- Source/MulticopterSim/dynamics/test/plotlog.py
+++ Source/MulticopterSim/dynamics/test/plotlog.py
@@ -5,20 +5,13 @@ LOGFILE = 'C:\\Users\sim\Desktop\log.csv'
 
 data = np.genfromtxt(LOGFILE, delimiter=',')
 
-t = data[:,0]
+labels = ['acc', 'vel', 'pos']
 
-plt.subplot(3,1,1)
-plt.plot(t,data[:,1])
-plt.ylabel("Z''")
+for k in range(3):
 
-plt.subplot(3,1,2)
-plt.plot(t, data[:,2])
-plt.ylabel("Z'")
-plt.ylim(plt.ylim()[::-1])
-
-plt.subplot(3,1,3)
-plt.plot(t, data[:,3])
-plt.ylabel("Z");
-plt.ylim(plt.ylim()[::-1])
+    plt.subplot(3,1,k+1)
+    plt.plot(data[:,0],data[:,k+1])
+    plt.ylim(plt.ylim()[::-1])
+    plt.ylabel(labels[k])
 
 plt.show()
