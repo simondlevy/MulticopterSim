ae4f0278deba87ebd0eb895cf41d19e252f521ec
Your Name <you@example.com>
2019-06-23 16:45:41 +0300

    Added labels

--- Extras/joytest/joytest.cpp
+++ Extras/joytest/joytest.cpp
@@ -12,13 +12,14 @@
 int main(int argc, char ** argv)
 {
     Joystick js;
-    float axes[6] = {0};
+    float axes[8] = {0};
     uint8_t buttons = 0;
 
     while (true) {
 
         if (!js.poll(axes, buttons)) {
-            printf("%f %f %f %f %f %f\n", axes[0], axes[1], axes[2], axes[3], axes[4], axes[5]);
+
+            printf("thr:%f rol:%f pit:%f yaw:%f au1:%f au2:%f\n", axes[0], axes[1], axes[2], axes[3], axes[4], axes[5]);
         }
     }
 
