c7a830601a030a261e9c95d946508d7bd1faf985
simondlevy <simon.d.levy@gmail.com>
2019-06-28 16:05:21 +0300

    More FlightManager work

--- Extras/matlab/FlightManager.java
+++ Extras/matlab/FlightManager.java
@@ -1,6 +1,28 @@
+import java.io.*;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+
 class FlightManager {
+
+    private static String HOST = "127.0.0.1";
+    private static short  PORT = 5000;
 
     public static void main(String [] args)
     {
+        try {
+            InetAddress addr = InetAddress.getByName(HOST);
+            DatagramSocket socket = new DatagramSocket();
+            //socket.setReuseAddress(true);
+            byte [] bytes = new byte[32];
+            DatagramPacket packet = new DatagramPacket(bytes, 32, addr, PORT);
+
+            for (int i=0; i<100000; ++i) {
+                socket.send(packet);
+            }
+
+        } catch (Exception e) {
+            System.err.println(e);
+        }
     }
 }
