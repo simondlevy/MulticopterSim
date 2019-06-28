f3d119cb69ee7ec74d0e70d00db691b10fb88b80
unknown <simondlevy>
2019-06-28 13:29:23 +0300

    More renaming

--- Source/MulticopterSim.Target.cs
+++ Source/MulticopterSim.Target.cs
@@ -15,6 +15,6 @@ public class MulticopterSimTarget : TargetRules
 	{
 		Type = TargetType.Game;
 
-		ExtraModuleNames.AddRange( new string[] { "MulticopterSim", "SimPlugin" } );
+		ExtraModuleNames.AddRange( new string[] { "MulticopterSim", "FlightModule" } );
 	}
 }
