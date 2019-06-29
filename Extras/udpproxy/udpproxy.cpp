369cf6fc77c1e9b24722ccdd58f852d482be22cb
simondlevy <simon.d.levy@gmail.com>
2019-06-29 20:57:50 +0300

    Begin adding dynamics

--- Extras/udpproxy/udpproxy.cpp
+++ Extras/udpproxy/udpproxy.cpp
@@ -11,9 +11,12 @@
 #include "sockets/UdpServerSocket.hpp"
 #include "sockets/UdpClientSocket.hpp"
 
-static const char * HOST = "127.0.0.1";
-static const short MOTOR_PORT = 5000;
-static const short TELEM_PORT = 5001;
+#include "MultirotorDynamics.hpp"
+
+static const char * HOST       = "127.0.0.1";
+static const short  MOTOR_PORT = 5000;
+static const short  TELEM_PORT = 5001;
+static const double DELTA_T    = 0.001;
 
 int main(int argc, char ** argv)
 {
@@ -22,20 +25,25 @@ int main(int argc, char ** argv)
 
     int count = 0;
 
+    double time = 0;
+
     while (true) {
 
-        double motorvals[4];
+        double motorvals[4] = {0};
 
         motorServer.receiveData(motorvals, 32);
 
-        uint64_t tmp;
-        memcpy(&tmp, &motorvals[1], 8);
+        // Time Gyro, Quat, Location, Rotation
+        double telemetry[14];
 
-        double telem = 99;
-        telemClient.sendData(&telem, sizeof(double));
+        telemetry[0] = time;
+
+        telemClient.sendData(telemetry, sizeof(telemetry));
 
         printf("%06d: %f %f %f %f\n", 
                 count++, motorvals[0], motorvals[1], motorvals[2], motorvals[3]);
+
+        time += DELTA_T;
     }
 
     motorServer.closeConnection();
