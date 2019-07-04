bf86fc67c07f8bf60c2568a892fd3c1e63469a65
simondlevy <simon.d.levy@gmail.com>
2019-07-04 12:00:25 +0300

    =More Goshawk work

--- Source/MulticopterSim/pawns/Goshawk.cpp
+++ Source/MulticopterSim/pawns/Goshawk.cpp
@@ -34,15 +34,15 @@ AGoshawkPawn::AGoshawkPawn()
 
     Vehicle::build(objects);
 
-    Vehicle::addMotor(objects, 0, +0.60, +1.5, _layout, Prop1Statics.mesh.Get());
+    Vehicle::addMotor(objects, 2, +0.60, +1.5, _layout, Prop3Statics.mesh.Get());
+    Vehicle::addMotor(objects, 7, +0.60, -1.5, _layout, Prop8Statics.mesh.Get());
 
+    Vehicle::addMotor(objects, 0, +100, -100, _layout, Prop1Statics.mesh.Get());
     Vehicle::addMotor(objects, 1, -100, -100, _layout, Prop2Statics.mesh.Get());
-    Vehicle::addMotor(objects, 2, +100, -100, _layout, Prop3Statics.mesh.Get());
     Vehicle::addMotor(objects, 3, -100, +100, _layout, Prop4Statics.mesh.Get());
     Vehicle::addMotor(objects, 4, -100, +100, _layout, Prop5Statics.mesh.Get());
     Vehicle::addMotor(objects, 5, -100, +100, _layout, Prop6Statics.mesh.Get());
     Vehicle::addMotor(objects, 6, -100, +100, _layout, Prop7Statics.mesh.Get());
-    Vehicle::addMotor(objects, 7, -100, +100, _layout, Prop8Statics.mesh.Get());
 
     _vehicle = new OctoXAP(objects, _params);
 }
