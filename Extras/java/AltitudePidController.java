3ed6e55600579f437ff67e3bec4627f999890ee3
simondlevy <simon.d.levy@gmail.com>
2019-07-13 21:48:37 +0300

    =More Java

--- Extras/java/AltitudePidController.java
+++ Extras/java/AltitudePidController.java
@@ -8,44 +8,28 @@ MIT License
 
 public class AltitudePidController {
 
+    public AltitudePidController(double target, double posP, double velP, double velD, double windupMax)
+    {
+        construct(target, posP, velP, velD, windupMax);
+    }
+
+    public AltitudePidController(double target, double posP, double velP, double velD)
+    {
+        construct(target, posP, velP, velD, 10.0);
+    }
+
+    private void construct(double target, double posP, double velP, double velD, double windupMax)
+    {
+        _target = target;
+        _posP = posP;
+        _velP = velP;
+        _velD = velD;
+        _windupMax = windupMax;
+    }
+
+    private double _target;
+    private double _posP;
+    private double _velP;
+    private double _velD;
+    private double _windupMax;
 }
-
-/*
-    def __init__(self, target, posP, velP, velI, velD, windupMax=10):
-
-        //g In a real PID controller, this would be a set-point
-        self.target = target
-
-        //g Constants
-        self.posP = posP
-        self.velP = velP
-        self.velI = velI
-        self.velD = velD
-        self.windupMax = windupMax
-
-        //g Values modified in-flight
-        self.posTarget      = 0
-        self.lastError      = 0
-        self.integralError  = 0
-        self.altitudeTarget = 0
-
-    def u(self, alt, vel, dt):
-
-        //g compute dzdt setpoint and error
-        velTarget = (self.target - alt) * self.posP
-        velError = velTarget - vel
-
-        //g Update error integral and error derivative
-        self.integralError +=  velError * dt
-        self.integralError = AltitudePidController._constrainAbs(self.integralError + velError * dt, self.windupMax)
-        deltaError = (velError - self.lastError) / dt if abs(self.lastError) > 0 else 0
-        self.lastError = velError
-
-        //g Compute control u
-        return self.velP * velError + self.velD * deltaError + self.velI * self.integralError
-
-    def _constrainAbs(x, lim):
-
-        return -lim if x < -lim else (+lim if x > +lim else x)
-
-*/
