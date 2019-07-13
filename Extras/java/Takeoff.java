611887f43d0208654f8bd4bf1cb3b4b87ee30393
simondlevy <simon.d.levy@gmail.com>
2019-07-13 22:05:55 +0300

    =More Java

--- Extras/java/Takeoff.java
+++ Extras/java/Takeoff.java
@@ -8,8 +8,27 @@
 
 public class Takeoff {
 
+    // Target params
+    static final double ALTITUDE_START  = 0;
+    static final double ALTITUDE_TARGET = 10;
+    static final double VARIO_TOLERANCE = .01; //g level-off velocity
+
+    // PID params
+    static final double ALT_P = 1.25;
+    static final double VEL_P = 1.5;
+    static final double VEL_I = 1.0;
+    static final double VEL_D = 0.05;
+
     public static void main(String [] args)
     {
+
+        // initial conditions
+        double z = 0;
+        double zprev = 0;
+        double tprev = 0;
+        double dzdt = 0;
+        double u = 0;
+
         Multicopter copter = new Multicopter("127.0.0.1", 5000, 5001);
 
         copter.start();
