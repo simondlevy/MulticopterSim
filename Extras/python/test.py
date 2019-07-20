eb5a7514043f249344a985d8d3e8cec7b7def0f2
unknown <simondlevy>
2019-07-20 18:47:03 +0300

    More Python work

--- Extras/python/test.py
+++ Extras/python/test.py
@@ -6,9 +6,14 @@ import numpy as np
 motorSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 motorSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
 
+telemSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+telemSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+
+telemSocket.bind(('127.0.0.1', 5001))
+
 while True:
 
-    motorSocket.sendto(np.ndarray.tobytes(np.array([0.6,0.6,0.6,0.6])), ('127.0.0.1', 5000))
+    motorSocket.sendto(np.ndarray.tobytes(0.55*np.ones(4)), ('127.0.0.1', 5000))
 
-
+    telemSocket.recvfrom(80)
 
