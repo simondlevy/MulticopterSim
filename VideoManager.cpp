bd8d4246645397b2c25102bc6e93b4c6294996c8
unknown <simondlevy>
2019-07-21 19:17:55 +0300

    Stubbed VideoManager routines

---
+++ VideoManager.cpp
@@ -0,0 +1,21 @@
+/*
+   Null VideoManager routines
+
+   Copyright(C) 2019 Simon D.Levy
+
+   MIT License
+*/
+
+FLIGHTMODULE_API void videoManagersStart(UTextureRenderTarget2D * cameraRenderTarget1,UTextureRenderTarget2D * cameraRenderTarget2)
+{
+    (void)cameraRenderTarget1;
+    (void)cameraRenderTarget2;
+}
+
+FLIGHTMODULE_API void videoManagersStop(void)
+{
+}
+
+FLIGHTMODULE_API void videoManagersGrabImages(void)
+{
+}
