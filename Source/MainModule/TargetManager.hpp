8c463a268427a2089b3e982ed658de9f73b43ee7
Yam Geva <35561566+yamgeva@users.noreply.github.com>
2019-07-31 13:46:23 +0300

    Update TargetManager.hpp
    added target rotation

--- Source/MainModule/TargetManager.hpp
+++ Source/MainModule/TargetManager.hpp
@@ -7,23 +7,30 @@ class FTargetManager : public FThreadedManager {
 protected:
 
 	FVector _location;
-
+	FRotator _rotation;
+	
 	virtual void performTask(double currentTime) override
 	{
-		computeLocation(currentTime);
+		computeState(currentTime);
 	}
 
 	FTargetManager() : FThreadedManager()
 	{
 		_location = FVector(0, 10, 0);
+		_rotation = FVector(0, 0, 0);
 	}
 
-	virtual void computeLocation(double currentTime) = 0;
+	virtual void computeState(double currentTime) = 0;
 
 public:
 
 	const FVector & getLocation(void)
 	{
 		return _location;
+	}
+	
+	const FRotator & getRotation(void)
+	{
+		return _rotation;
 	}
 };
