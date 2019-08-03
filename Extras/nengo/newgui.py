5d02faaaabb586156f898839e0d035163770e5ae
simondlevy <simon.d.levy@gmail.com>
2019-08-03 18:05:46 +0300

    =More Nengo

--- Extras/nengo/newgui.py
+++ Extras/nengo/newgui.py
@@ -10,6 +10,12 @@ KP = 0.4
 KD = 10.0
 KI = 0.03
 
+# Network params
+SIM_TIME         = 0.001
+N_NEURONS        = 200
+INTEGRAL_SYNAPSE = 0.1
+INTEGRAL_RADIUS  = 2
+
 # Create a multicopter and start communications with simulator
 copter = Multicopter()
 copter.start()
@@ -27,6 +33,19 @@ with model:
     q_target = nengo.Node(lambda t: q_target_value, label='q_target')
     q = nengo.Node(lambda t: q_value, label='q')
     dq = nengo.Node(lambda t: dq_value, label='dq')
+
+    dq_target = nengo.Node(None, size_in=1, label='dq_target')
+    nengo.Connection(q_target, dq_target, synapse=None, transform=1) # direct feed-forward synapse
+    nengo.Connection(q_target, dq_target, synapse=0, transform=-1)   # minimal time-step delay
+
+    q_err = nengo.Ensemble(n_neurons=N_NEURONS, dimensions=1, label='q_err')
+    nengo.Connection(q_target, q_err, synapse=None)
+    nengo.Connection(q, q_err, synapse=None, transform=-1)
+
+    q_err_integral = nengo.Ensemble(n_neurons=N_NEURONS, dimensions=1, radius=INTEGRAL_RADIUS,
+            label='q_err_integral')
+    nengo.Connection(q_err, q_err_integral, synapse=INTEGRAL_SYNAPSE, transform=INTEGRAL_SYNAPSE)
+    nengo.Connection(q_err_integral, q_err_integral, synapse=INTEGRAL_SYNAPSE, transform=1)
 
     x = nengo.Ensemble(n_neurons=1200, dimensions=3, radius=30)
 
@@ -46,7 +65,7 @@ with model:
         telem = copter.getState()
 
         # Extract time from state
-        t =  telem[0]
+        #t =  telem[0]
 
         # Extract altitude from state.  Altitude is in NED coordinates, so we negate it to use as input
         # to PID controller.
