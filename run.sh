cde121bc50c42f0f78d2bf65fb66883cf60d3ae2
simondlevy <simon.d.levy@gmail.com>
2019-08-04 18:51:26 +0300

    Fixed Linux run script

--- run.sh
+++ run.sh
@@ -3,5 +3,9 @@
 #
 # Copyright (C) 2019 Simon D. Levy
 #
+
+# Change this to where you installed UnrealEngine
+UE4=$HOME/UnrealEngine
+
 # MIT License
-UE4Editor > /dev/null
+$UE4/Engine/Binaries/Linux/UE4Editor > /dev/null
