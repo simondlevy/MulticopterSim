575f369514d5d432a6fab96a9e59bd08d2c414c0
simondlevy <simon.d.levy@gmail.com>
2019-08-04 18:43:48 +0300

    Cleanup

--- Extras/nengo/gui_takeoff.py
+++ Extras/nengo/gui_takeoff.py
@@ -6,48 +6,22 @@ Copyright (C) 2019 Simon D. Levy
 MIT License
 '''
 
-import nengo
-import numpy as np
+from altitude_hold import buildpid, runpid, startcopter
 
-from multicopter_sim import Multicopter
-from nengo_pid_controller import NengoPidController
-
-# Target 
-ALTITUDE_TARGET = 10
-
-# PID params
-KP = 0.4
-KD = 10.0
-KI = 0.03
-
-model = nengo.Network()
+model, pid = buildpid()
 
 with model:
 
-    pid = NengoPidController(model, KP, KD, KI)
+    pass
 
 def on_step(sim):
     
     if not hasattr(on_step, 'copter'):
 
-        # Start the 'copter
-        on_step.copter = Multicopter()
-        on_step.copter.start()
+        on_step.copter = startcopter()
         
     if on_step.copter.isReady():
 
-        # Get 'copter state from sim
-        telem = on_step.copter.getState()
+        runpid(on_step.copter, pid)
 
-        # Extract altitude from state.  Altitude is in NED coordinates, so we negate it to use as input
-        # to PID controller.
-        z = -telem[9]
 
-        # Get correction from PID controller
-        u = pid.getCorrection(ALTITUDE_TARGET, z)[0]
-
-        # Constrain correction to [0,1] to represent motor value
-        u = max(0, min(1, u))
-
-        # Set motor values in sim
-        on_step.copter.setMotors(u*np.ones(4))
