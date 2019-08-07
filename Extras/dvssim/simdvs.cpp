1bc40e9ed8b3bb1f5952bfdf7d1af7b61a58cc3c
simondlevy <simon.d.levy@gmail.com>
2019-08-07 23:33:07 +0300

    Displaying empty image

--- Extras/dvssim/simdvs.cpp
+++ Extras/dvssim/simdvs.cpp
@@ -6,10 +6,24 @@
    MIT License
 */
 
+#include <opencv2/highgui/highgui.hpp>
+
 #include "Davis346Sim.hpp"
 
-int main(int argc, char ** argv)\
+static const uint16_t CAMERA_COLS = 346;
+static const uint16_t CAMERA_ROWS = 260;
+
+int main(int argc, char ** argv)
 {
+    while (true) {
+
+        cv::Mat cameraImage = cv::Mat::zeros(CAMERA_ROWS, CAMERA_COLS, CV_8UC3);
+
+        cv::imshow("EventCamera", cameraImage);
+
+        if (cv::waitKey(1) == 27) break; // quit on ESC
+    }
+
     return 0;
 }
 
