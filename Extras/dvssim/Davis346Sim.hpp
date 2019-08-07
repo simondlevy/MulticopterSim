4b50eaca18b2128e70a2a36f93287c65488b10bd
simondlevy <simon.d.levy@gmail.com>
2019-08-07 23:23:45 +0300

    More DVS work

---
+++ Extras/dvssim/Davis346Sim.hpp
@@ -0,0 +1,81 @@
+/*
+   Platform-indpendent simulator for iniVation DAVIS346 Dynamic Vision Sensor
+
+   Copyright(C) 2019 Simon D.Levy
+
+   MIT License
+*/
+
+#pragma once
+
+#include <queue>
+using namespace std;
+
+#include <stdint.h>
+
+#ifdef _WIN32
+#include "../MainModule/Utils.hpp"
+#else
+#include <stdio.h>
+#define debug printf
+#endif
+
+class Davis346 {
+
+    public:
+        
+        // Simplified AER event structure, based on
+        // https://github.com/SensorsINI/jaer/blob/master/src/net/sf/jaer/event/BasicEvent.java
+        typedef struct {
+
+            uint32_t timestamp; // us
+            uint16_t x;
+            uint16_t y;
+
+        } event_t;
+
+        typedef struct {
+
+            double x;
+            double y;
+            double z;
+
+        } location_t;
+
+    private:
+
+        // Field of view, assuming a lens with 4.5mm focal length. See:
+        // https://inivation.com/support/hardware/davis346/#computations-of-field-of-view
+        static constexpr double FOV_H = 70.8;
+        static constexpr double FOV_V = 56.2;
+
+        // Assume a spherical cow
+        double _targetSize = 0;
+
+        queue<event_t> _eventq;
+
+    public:
+        
+        Davis346(double targetSize)
+        {
+            _targetSize = targetSize;
+        }
+
+        ~Davis346(void)
+        {
+        }
+
+        void update(const location_t & vehicleLocation, const location_t & targetLocation)
+        {
+			debug("vehicle: %+3.3f %+3.3f %+3.3f    target: %+3.3f %+3.3f %+3.3f",
+				vehicleLocation.x, vehicleLocation.y, vehicleLocation.z,
+				targetLocation.x, targetLocation.y, targetLocation.z);
+        }
+
+        event_t dequeueEvent(void)
+        {
+            event_t event = _eventq.front();
+            _eventq.pop();
+            return event;
+        }
+}; 
---
+++ Extras/dvssim/simdvs.cpp
@@ -0,0 +1,16 @@
+/*
+   DVS simulator demo program
+
+   Copyright(C) 2019 Simon D.Levy
+
+   MIT License
+*/
+
+#include "Davis346Sim.hpp"
+
+int main(int argc, char ** argv)\
+{
+    return 0;
+}
+
+
