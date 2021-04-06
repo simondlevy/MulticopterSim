8cca17adb0af303d2100ce79dde96986821ada93
simondlevy <simon.d.levy@gmail.com>
2021-04-06 16:23:57 -0400

    More socket work

--- Extras/python/multicopter_sim/__init__.py
+++ Extras/python/multicopter_sim/__init__.py
@@ -12,25 +12,35 @@ from threading import Thread
 import socket
 import numpy as np
 
+
 class Multicopter(object):
     '''
-    Represents a Multicopter object communicating with MulticopterSim via UDP socket calls.
+    Represents a Multicopter object communicating with MulticopterSim via UDP
+    socket calls.
     '''
 
-    def __init__(self, host='127.0.0.1', motorPort=5000, telemetryPort=5001, motorCount=4):
+    # Time, followed by 12-dimensional state vector (Bouabdallah 2004)
+    STATE_SIZE = 13
+
+    def __init__(self, host='127.0.0.1', motorPort=5000, telemetryPort=5001,
+                 motorCount=4):
         '''
         Creates a Multicopter object.
         host - name of host running MulticopterSim
-        motorPort - port over which this object will send motor commands to host
-        telemeteryPort - port over which this object will receive telemetry  from host
+        motorPort - port over which this object will send motor commands to
+                    host
+        telemeteryPort - port over which this object will receive telemetry
+                         from host
         motorCount - number of motors in vehicle running in simulator on host
         '''
-     
+
         self.motorSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.motorSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+        self.motorSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,
+                                    True)
 
         self.telemSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.telemSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+        self.telemSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,
+                                    True)
 
         self.telemSocket.bind((host, telemetryPort))
 
@@ -42,7 +52,7 @@ class Multicopter(object):
         self.thread.daemon = True
 
         self.motorVals = np.zeros(motorCount)
-        self.state = np.zeros(13) # Time, followed by 12-dimensional state vector (Bouabdallah 2004)
+        self.state = np.zeros(self.STATE_SIZE)
 
         self.ready = False
 
@@ -54,13 +64,12 @@ class Multicopter(object):
         self.thread.start()
 
     def isReady(self):
-        
+
         return self.ready
 
     def getState(self):
         '''
-        Returns current vehicle state as an array of the form [time, gx, gy, gz, ax, ay, az, px, py, pz],
-        where g=gyro; a=accelerometer; p=position.
+        Returns current vehicle state
         '''
 
         return self.state
@@ -69,14 +78,14 @@ class Multicopter(object):
         '''
         Sets motor values between 0 and 1.
         '''
- 
+
         self.motorVals = np.copy(motorVals)
 
     def _run(self):
 
         while True:
 
-            data, _ = self.telemSocket.recvfrom(80)
+            data, _ = self.telemSocket.recvfrom(8*self.STATE_SIZE)
             self.state = np.frombuffer(data)
 
             self.ready = True
@@ -86,5 +95,5 @@ class Multicopter(object):
                 self.telemSocket.close()
                 break
 
-            self.motorSocket.sendto(np.ndarray.tobytes(self.motorVals), (self.host, self.motorPort))
-
+            self.motorSocket.sendto(np.ndarray.tobytes(self.motorVals),
+                                    (self.host, self.motorPort))
