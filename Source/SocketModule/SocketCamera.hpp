8942c71956a0b8aed57a5f6736acc726d0cd6cfb
simondlevy <simon.d.levy@gmail.com>
2021-04-26 20:31:22 -0400

    Sending some image bytes over socket [Issue 105]

--- Source/SocketModule/SocketCamera.hpp
+++ Source/SocketModule/SocketCamera.hpp
@@ -10,13 +10,22 @@
 
 #include "../MainModule/Camera.hpp"
 
+#include "../../Extras/sockets/UdpClientSocket.hpp"
+
 class SocketCamera : public Camera {
 
     private:
 
+        // Comms
+        static constexpr char * HOST = "127.0.0.1"; // localhost
+        static constexpr uint16_t IMAGE_PORT = 5002;
+
         // Camera params
         static constexpr Resolution_t RES = RES_640x480;
         static constexpr float FOV = 135;
+
+        // Create one-way server for images out
+        UdpClientSocket imageUdp = UdpClientSocket(HOST, IMAGE_PORT);
 
     public:
 
@@ -29,7 +38,8 @@ class SocketCamera : public Camera {
 
         virtual void processImageBytes(uint8_t * bytes) override
         { 
-            debug("Process image bytes");
+            // Send image data
+            imageUdp.sendData(bytes, 40000);
         }
 
 }; // Class SocketCamera
