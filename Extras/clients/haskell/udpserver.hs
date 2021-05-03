c55b39a8dca5ec1a6f73e4f51c4e8595a62c64c1
simondlevy <simon.d.levy@gmail.com>
2021-05-03 13:14:33 -0400

    Cleanup

--- Extras/clients/haskell/udpserver.hs
+++ Extras/clients/haskell/udpserver.hs
@@ -5,18 +5,6 @@ import Network.Socket.ByteString
 import Network.BSD
 import Data.List
 import Data.ByteString.Internal
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Char8 as C
-import qualified Data.Text as T
-import Data.Text.Encoding (encodeUtf8)
-import Data.Char (ord)
-
--- https://stackoverflow.com/questions/3232074/what-is-the-best-way-to-convert-string-to-bytestring
-packStr, packStr', packStr'' :: String -> B.ByteString
-packStr   = B.pack . map (fromIntegral . ord)
-packStr'  = C.pack
-packStr'' = encodeUtf8 . T.pack
-
 
 type HandlerFunc = SockAddr -> Data.ByteString.Internal.ByteString -> IO ()
 
