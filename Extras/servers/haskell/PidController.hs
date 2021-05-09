816e33e19b930dacf9c09aa527a869bc0beb321c
simondlevy <simon.d.levy@gmail.com>
2021-05-09 15:42:45 -0400

    =More Haskell work

--- Extras/servers/haskell/PidController.hs
+++ Extras/servers/haskell/PidController.hs
@@ -22,11 +22,6 @@ pid_kd (PidController _kp _ki kd _windupMax) = kd
 pid_windup_max :: PidController -> Double
 pid_windup_max (PidController _kp _ki _kd windupMax) = windupMax
 
-data PidController' = PidController' PidController Double Double Double deriving Show
-
-pid'_kp :: PidController' -> Double
-pid'_kp (PidController' pidController _windupMax _lastError _integralError) = pid_kp pidController
-
-pid'_kd :: PidController' -> Double
-pid'_kd (PidController' pidController _windupMax _lastError _integralError) = pid_kd pidController
+makePidController :: Double -> Double -> Double -> PidController
+makePidController kp ki kd = PidController kp ki kd 0
 
