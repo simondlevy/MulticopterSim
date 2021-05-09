f227f361b561dfc57f177a2e78704d3ecf91e566
simondlevy <simon.d.levy@gmail.com>
2021-05-09 15:45:15 -0400

    =More Haskell work

--- Extras/servers/haskell/PidController.hs
+++ Extras/servers/haskell/PidController.hs
@@ -8,20 +8,19 @@
 
 module PidController (PidController) where
 
-data PidController = PidController Double Double Double Double deriving Show
+data PidController = PidController Double Double Double Double Double Double deriving Show
 
 pid_kp :: PidController -> Double
-pid_kp (PidController kp _ki _kd _windupMax) = kp
+pid_kp (PidController kp _ki _kd _windupMax _lastError _errorIntegral) = kp
 
 pid_ki :: PidController -> Double
-pid_ki (PidController _kp ki _kd _windupMax) = ki
+pid_ki (PidController _kp ki _kd _windupMax _lastError _errorIntegral) = ki
 
 pid_kd :: PidController -> Double
-pid_kd (PidController _kp _ki kd _windupMax) = kd
+pid_kd (PidController _kp _ki kd _windupMax _lastError _errorIntegral) = kd
 
 pid_windup_max :: PidController -> Double
-pid_windup_max (PidController _kp _ki _kd windupMax) = windupMax
+pid_windup_max (PidController _kp _ki _kd windupMax _lastError _errorIntegral) = windupMax
 
 makePidController :: Double -> Double -> Double -> PidController
-makePidController kp ki kd = PidController kp ki kd 0
-
+makePidController kp ki kd = PidController kp ki kd 0 0 0
