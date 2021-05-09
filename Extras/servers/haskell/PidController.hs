67ee23f40df37e817f9e2f09d1e4b671608ff6e1
simondlevy <simon.d.levy@gmail.com>
2021-05-09 14:08:28 -0400

    =More Haskell work

--- Extras/servers/haskell/PidController.hs
+++ Extras/servers/haskell/PidController.hs
@@ -9,8 +9,14 @@
 module PidController (PidController) where
 
 
-data PidController = PidController
-  { kp :: Double
-  , ki :: Double
-  , kd :: Double }
+data PidController = PidController Double Double Double deriving Show
+
+pid_kp :: PidController -> Double
+pid_kp (PidController kp _ki _kd) = kp
+
+pid_ki :: PidController -> Double
+pid_ki (PidController _kp ki _kd) = ki
+
+pid_kd :: PidController -> Double
+pid_kd (PidController _kp _ki kd) = kd
 
