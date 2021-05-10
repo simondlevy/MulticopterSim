cc916d4fa6e1b6f8892ebda07279106402cc457d
simondlevy <simon.d.levy@gmail.com>
2021-05-10 13:23:56 -0400

    =More Haskell work

--- Extras/servers/haskell/takeoff.hs
+++ Extras/servers/haskell/takeoff.hs
@@ -8,6 +8,7 @@ import Types
 import Multicopter
 import AltitudeController
 
+{--
 takeoffControl :: ClosedLoopController
 takeoffControl _time state = 
     let altitude = -(state_z state) -- NED => ENU
@@ -15,7 +16,8 @@ takeoffControl _time state =
         err = target - altitude
         u = 0.5 - 0.00001 * err
     in Demands u 0 0 0
+--}
 
 main :: IO ()
 main = do
-    runMulticopter takeoffControl quadXAPMixer
+    runMulticopter (altitudeController 10.0 1.0 1.0 0.0 10.0) quadXAPMixer
