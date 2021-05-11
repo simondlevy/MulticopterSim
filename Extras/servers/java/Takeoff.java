587c3a30caa76f9c7492b661ff00d870b45702ff
simondlevy <simon.d.levy@gmail.com>
2021-05-11 00:56:25 -0400

    Cleanup

--- Extras/servers/java/Takeoff.java
+++ Extras/servers/java/Takeoff.java
@@ -19,10 +19,7 @@ public class Takeoff {
     public static void main(String [] args)
     {
         // initial conditions
-        double z = 0;
-        double zprev = 0;
         double tprev = 0;
-        double dzdt = 0;
         double u = 0;
 
         // Create PID controller
@@ -46,18 +43,20 @@ public class Takeoff {
             // Get state from sim
             double [] state = copter.getVehicleState();
 
-            // Extract altitude from state.  Altitude is in NED coordinates, so we negate it to use as input to PID controller.
-            z = -state[Multicopter.STATE_Z];
+            // Extract altitude and its first derivative from state.  Altitude
+            // is in NED coordinates, so we negate it to use as input to PID
+            // controller.
+            double z = -state[Multicopter.STATE_Z];
+            double dz = -state[Multicopter.STATE_DZ];
 
             // Compute vertical climb rate as first difference of altitude over time
             if (t > tprev) {
 
                 // Use temporal first difference to compute vertical velocity
                 double dt = t - tprev;
-                dzdt = (z-zprev) / dt;
 
                 // Get correction from PID controller
-                u = pid.u(z, dzdt, dt);
+                u = pid.u(z, dz, dt);
 
                 // Constrain correction to [0,1] to represent motor value
                 u = Math.max(0., Math.min(1., u));
@@ -66,8 +65,7 @@ public class Takeoff {
             // Set motor values in sim
             copter.setMotors(ones(u, 4));
 
-            // Update for first difference
-            zprev = z;
+            // Update
             tprev = t;
 
             // Yield to Multicopter thread
