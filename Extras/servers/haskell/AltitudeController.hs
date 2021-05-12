6a2a803417ed08a2da0013a23c71bd3fc7b1e8b6
simondlevy <simon.d.levy@gmail.com>
2021-05-12 14:50:17 -0400

    More Haskell work

--- Extras/servers/haskell/AltitudeController.hs
+++ Extras/servers/haskell/AltitudeController.hs
@@ -10,6 +10,15 @@ module AltitudeController where
 
 import Types
 
+data AltitudeControllerConstants =
+    AltitudeControllerConstants {
+      altitude_target ::Double
+    , altitude_Kp_z ::Double
+    , altitude_Kp_dz ::Double
+    , altitude_Ki_dz ::Double
+    } deriving (Show)
+             
+
 altitudeController :: PidController
 
 altitudeController _time _vehicleState _demands _controllerState =
