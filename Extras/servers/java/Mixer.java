9684e169f70cf72d161e63e5f9f2a80e6a818775
simondlevy <simon.d.levy@gmail.com>
2021-05-19 15:03:33 -0400

    Finished simple mixer

--- Extras/servers/java/Mixer.java
+++ Extras/servers/java/Mixer.java
@@ -10,13 +10,7 @@
  */
 
 /**
-  * This is the mixer for a quadrotor laid out in the MultiWii configuration:
-
-    4cw   2ccw
-       \ /
-        ^
-       / \
-    3ccw  1cw
+  * This is the mixer for a quadrotor laid out in the ArduPilot configuration:
 
     Eventually this should be an abstract class allowing us to subclas various configurations
     (including Coaxial)
@@ -24,15 +18,27 @@
 */
 public class Mixer {
 
+    private static double [][] d = new double[4][4];
+
+    public Mixer()
+    {
+        //        Th            RR            PF            YR
+        d[0][0] = +1; d[0][1] = -1; d[0][2] = -1; d[0][3] = +1; // 1 right front
+        d[1][0] = +1; d[1][1] = +1; d[1][2] = +1; d[1][3] = +1; // 2 left rear
+        d[2][0] = +1; d[2][1] = +1; d[2][2] = -1; d[2][3] = -1; // 3 left front
+        d[3][0] = +1; d[3][1] = -1; d[3][2] = +1; d[3][3] = -1; // 4 right rear
+    }
+
     public double [] getMotors(double [] u)
     {
         double [] omega = new double[4];
 
-        // XXX Fake up a mixer by setting all motors to throttle demand
-        for (int k=0; k<4; ++k) {
-            omega[k] = u[0];
+        for (int i=0; i<4; ++i) {
 
+            omega[i] = (u[0] * d[i][0] + u[1] * d[i][1] +
+                        u[2] * d[i][2] + u[3] * d[i][3]);
         }
+
         return omega;
     }
 }
