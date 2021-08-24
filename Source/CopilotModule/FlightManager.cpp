091e9d85107e227bfdff30a41a5a3a6e5a8aa30f
simondlevy <simon.d.levy@gmail.com>
2021-08-24 17:06:46 -0400

    =More Haskell Copilot work

--- Source/CopilotModule/FlightManager.cpp
+++ Source/CopilotModule/FlightManager.cpp
@@ -17,22 +17,35 @@
 
 // Used by Copilot
 double copilot_receiverThrottle = 0;
-double copilot_altimeterZ = 0;
-double copilot_time = 0;
-double copilot_gyrometerX = 0;
 double copilot_receiverRoll = 0;
-double copilot_gyrometerY = 0;
 double copilot_receiverPitch = 0;
 double copilot_receiverYaw = 0;
+
+double copilot_altimeterZ = 0;
+
+double copilot_time = 0;
+
+double copilot_gyrometerX = 0;
+double copilot_gyrometerY = 0;
 double copilot_gyrometerZ = 0;
+
 double copilot_quaternionW = 0;
 double copilot_quaternionX = 0;
 double copilot_quaternionY = 0;
 double copilot_quaternionZ = 0;
 
+static double _m1;
+static double _m2;
+static double _m3;
+static double _m4;
+
 // Called by Copilot
 void copilot_runMotors(double m1, double m2, double m3, double m4)
 {
+    _m1 = m1;
+    _m2 = m2;
+    _m3 = m3;
+    _m4 = m4;
 }
 
 FCopilotFlightManager::FCopilotFlightManager(Dynamics * dynamics)
@@ -49,7 +62,9 @@ void FCopilotFlightManager::getActuators(const double time, double * values)
     // Call Copilot
     step();
 
-    for (uint8_t i=0; i<_dynamics->rotorCount(); ++i) {
-        values[i] = 0.6;
-    }
+    values[0] = _m1;
+    values[1] = _m2;
+    values[2] = _m3;
+    values[3] = _m4;
+
 }
