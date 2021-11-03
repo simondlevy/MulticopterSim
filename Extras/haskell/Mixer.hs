8515b5f751e8be68ac3a7ba91be1a7a0090e34b2
simondlevy <simon.d.levy@gmail.com>
2021-11-03 16:31:43 -0400

    Cleanup

--- Extras/haskell/Mixer.hs
+++ Extras/haskell/Mixer.hs
@@ -21,14 +21,7 @@ data Motors = QuadMotors { m1 :: SFloat
                          , m3 :: SFloat
                          , m4 :: SFloat }
 
-addMotors :: Motors -> Motors -> Motors
-
-addMotors (QuadMotors m11 m12 m13 m14) (QuadMotors m21 m22 m23 m24) 
-  = QuadMotors (m11 + m21) (m12 + m22) (m13 + m23) (m14 + m24)
-
 type Mixer = Safety -> Demands -> Motors
-
--- addMotors :: Motors -> Motors -> Motors
 
 quadXMWMixer :: Mixer
 
