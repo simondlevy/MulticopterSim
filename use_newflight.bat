7a67b1011b2901196784addceb928d96d26e99b0
simondlevy <simon.d.levy@gmail.com>
2022-05-31 18:04:19 -0400

    4.26 => 5.0

---
+++ use_newflight.bat
@@ -0,0 +1,10 @@
+::
+:: Socket module setup script for MulticopterSim
+::
+:: Copyright (C) 2021 Simon D. Levy
+::
+:: MIT License
+
+copy Extras\modules\MulticopterSim_newflight.uproject MulticopterSim.uproject
+copy Extras\modules\MulticopterSim_newflight.Target.cs Source\MulticopterSim.Target.cs
+copy Extras\modules\MulticopterSimEditor_newflight.Target.cs Source\MulticopterSimEditor.Target.cs
