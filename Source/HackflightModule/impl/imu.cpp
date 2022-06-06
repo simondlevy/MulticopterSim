68c1fccbb65a719e2eb0d4caa10de67ded99caee
simondlevy <simon.d.levy@gmail.com>
2022-06-05 22:22:30 -0400

    Fixed gyro Y axis simulation

--- Source/HackflightModule/impl/imu.cpp
+++ Source/HackflightModule/impl/imu.cpp
@@ -1,29 +1,52 @@
 #include <stdint.h>
 
+#define _USE_MATH_DEFINES
+#include <math.h>
+
 #include <time.h>
 #include <datatypes.h>
+#include <gyro.h>
+#include <rad2deg.h>
+
+#include "../../MainModule/Dynamics.hpp"
+
+static Dynamics * _dynamics;
+
+void shareDynamics(Dynamics * dynamics)
+{
+    _dynamics = dynamics;
+}
 
 extern "C" {
 
-void imuAccumulateGyro(float * adcf)
-{
-    (void)adcf;
-}
+    static float angle(uint8_t index)
+    {
+        return rad2deg(_dynamics->x(index));
+    }
 
-void imuGetQuaternion(uint32_t time, bool armed, quaternion_t * quat)
-{
-    // XXX stubbed for now
-    quat->w = 1;
-    quat->x = 0;
-    quat->y = 0;
-    quat->z = 0;
-}
+    void gyroReadScaled(vehicle_state_t * vstate, bool * isCalibrating)
+    {
+        vstate->dphi   =  angle(Dynamics::STATE_DPHI);
+        vstate->dtheta = -angle(Dynamics::STATE_DTHETA); // Nose-down positive
+        vstate->dpsi   =  angle(Dynamics::STATE_DPSI);
 
-void imuUpdateFusion(timeUs_t time, quaternion_t * quat, rotation_t * rot)
-{
-    (void)time;
-    (void)quat;
-    (void)rot;
-}
+        *isCalibrating = false;
+    }
+
+    void imuAccumulateGyro(float * adcf)
+    {
+        (void)adcf;
+    }
+
+    void imuGetEulerAngles(timeUs_t time, vehicle_state_t * vstate, bool armed)
+    {
+        (void)time;
+        (void)armed;
+
+        vstate->phi   =  angle(Dynamics::STATE_PHI);
+        vstate->theta = -angle(Dynamics::STATE_THETA); // Nose-down positive
+        vstate->psi   =  angle(Dynamics::STATE_PSI);
+
+    }
 
 } // extern "C"
