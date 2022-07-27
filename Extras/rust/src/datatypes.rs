aeb4d0a37c801015dee8c15c08c8c844a780f566
simondlevy <simon.d.levy@gmail.com>
2022-07-27 01:51:31 -0400

    =More Rust work

--- Extras/rust/src/datatypes.rs
+++ Extras/rust/src/datatypes.rs
@@ -1,11 +1,19 @@
 pub mod datatypes {
 
     #[repr(C)]
-    pub struct AltHoldPid{
+    pub struct AltHoldPid {
 
         pub error_integral: f32,
-        pub in_band:        bool,
-        pub target:         f32,
-        pub throttle:       f32
+        pub in_band: bool,
+        pub target:  f32,
+        pub throttle:f32
     }
+
+    #[repr(C)]
+    pub struct Demands {
+        pub throttle: f32,
+        pub roll: f32,
+        pub pitch: f32,
+        pub yaw: f32
+    } 
 }
