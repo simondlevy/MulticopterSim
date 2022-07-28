d84efed80493b871f24dc43d9157797c9535abb1
simondlevy <simon.d.levy@gmail.com>
2022-07-28 13:35:24 -0400

    =More Rust work

--- Extras/rust/src/hackflight.rs
+++ Extras/rust/src/hackflight.rs
@@ -1,10 +1,49 @@
 pub mod hackflight {
 
     use datatypes::datatypes::Motors;
+    use datatypes::datatypes::Hackflight;
+    use datatypes::datatypes::Demands;
+    use datatypes::datatypes::AltHoldPid;
 
-    pub fn run_hackflight() -> Motors {
+    use alt_hold::alt_hold::run_alt_hold;
 
-        Motors {m1: 0.0, m2:0.0, m3:0.0, m4:0.0}
-    }
+    pub fn run_hackflight(hackflight: Hackflight) -> Hackflight {
 
-}
+        let demands = hackflight.demands;
+        let vehicle_state = hackflight.vehicle_state;
+        let alt_hold_pid = hackflight.alt_hold_pid;
+
+        let (new_throttle, new_alt_hold_pid) = run_alt_hold(
+            demands.throttle,
+            -vehicle_state.z,  // NED => ENU
+            -vehicle_state.dz, // NED => ENU
+            AltHoldPid { 
+                error_integral: alt_hold_pid.error_integral,
+                in_band: alt_hold_pid.in_band,
+                target: alt_hold_pid.target
+            });
+
+        let new_demands = Demands {
+            throttle: new_throttle,
+            roll: 0.0,
+            pitch: 0.0,
+            yaw: 0.0
+        };
+
+        let new_motors = Motors {
+            m1: new_demands.throttle,
+            m2: new_demands.throttle,
+            m3: new_demands.throttle,
+            m4: new_demands.throttle
+        };
+
+        Hackflight {
+            demands: new_demands,
+            vehicle_state: vehicle_state,
+            alt_hold_pid: new_alt_hold_pid,
+            motors: new_motors
+        }
+
+    } // run_hackflight
+
+} // mod hackflight
--- Extras/rust/src/lib.rs
+++ Extras/rust/src/lib.rs
@@ -1,66 +1,63 @@
-use datatypes::datatypes::Hackflight;
-use datatypes::datatypes::Demands;
-use datatypes::datatypes::VehicleState;
 use datatypes::datatypes::AltHoldPid;
+use datatypes::datatypes::Demands;
+use datatypes::datatypes::Hackflight;
 use datatypes::datatypes::Motors;
+use datatypes::datatypes::VehicleState;
 
-//use hackflight::hackflight::run_hackflight;
-use alt_hold::alt_hold::run_alt_hold;
+use hackflight::hackflight::run_hackflight;
 
 pub mod datatypes;
-//pub mod hackflight;
+pub mod hackflight;
 pub mod alt_hold;
 
 #[no_mangle]
 pub extern "C" fn rust_run_hackflight(c_hackflight: *mut Hackflight) -> Hackflight {
 
-    let demands = unsafe { &(*c_hackflight).demands };
-    let vehicle_state = unsafe { &(*c_hackflight).vehicle_state };
-    let alt_hold_pid = unsafe { &(*c_hackflight).alt_hold_pid };
+    let c_demands = unsafe { &(*c_hackflight).demands };
+    let c_vehicle_state = unsafe { &(*c_hackflight).vehicle_state };
+    let c_alt_hold_pid = unsafe { &(*c_hackflight).alt_hold_pid };
 
-    let new_vehicle_state = VehicleState {
-        x: 0.0,
-        dx: 0.0,
-        y: 0.0,
-        dy: 0.0,
-        z: 0.0,
-        dz: 0.0,
-        phi: 0.0,
-        dphi: 0.0,
-        theta: 0.0,
-        dtheta: 0.0,
-        psi: 0.0,
-        dpsi: 0.0
+    let vehicle_state = VehicleState {
+        x: c_vehicle_state.x,
+        dx: c_vehicle_state.dx,
+        y: c_vehicle_state.y,
+        dy: c_vehicle_state.dy,
+        z: c_vehicle_state.z,
+        dz: c_vehicle_state.dz,
+        phi: c_vehicle_state.phi,
+        dphi: c_vehicle_state.dphi,
+        theta: c_vehicle_state.theta,
+        dtheta: c_vehicle_state.dtheta,
+        psi: c_vehicle_state.psi,
+        dpsi: c_vehicle_state.dpsi
     };
 
-    let (new_throttle, new_alt_hold_pid) = run_alt_hold(
-        demands.throttle,
-        -vehicle_state.z,  // NED => ENU
-        -vehicle_state.dz, // NED => ENU
-        AltHoldPid { 
-            error_integral: alt_hold_pid.error_integral,
-            in_band: alt_hold_pid.in_band,
-            target: alt_hold_pid.target
-        });
-
-    let new_demands = Demands {
-        throttle: new_throttle,
-        roll: 0.0,
-        pitch: 0.0,
-        yaw: 0.0
+    let demands = Demands { 
+        throttle: c_demands.throttle,
+        roll: c_demands.roll,
+        pitch: c_demands.pitch,
+        yaw: c_demands.yaw
     };
 
-    let new_motors = Motors {
-        m1: new_demands.throttle,
-        m2: new_demands.throttle,
-        m3: new_demands.throttle,
-        m4: new_demands.throttle
+    let alt_hold_pid = AltHoldPid {
+        error_integral: c_alt_hold_pid.error_integral,
+        in_band: c_alt_hold_pid.in_band,
+        target: c_alt_hold_pid.target
     };
 
-    Hackflight { 
-        demands: new_demands,
-        vehicle_state: new_vehicle_state,
-        alt_hold_pid: new_alt_hold_pid,
-        motors: new_motors
-    }
+    let motors = Motors {
+        m1: 0.0,
+        m2: 0.0,
+        m3: 0.0,
+        m4: 0.0
+    };
+
+    let hackflight = Hackflight { 
+        demands: demands,
+        vehicle_state: vehicle_state,
+        alt_hold_pid: alt_hold_pid,
+        motors: motors
+    };
+
+    run_hackflight(hackflight)
 }
