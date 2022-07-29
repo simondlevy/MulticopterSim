1812e0826123b6386b2b160133b94db67dc9c18b
simondlevy <simon.d.levy@gmail.com>
2022-07-29 01:58:56 -0400

    More Rust work

--- Extras/rust/src/main.rs
+++ Extras/rust/src/main.rs
@@ -7,8 +7,18 @@ pub mod datatypes;
 fn main() {
 
     let _vehicle_state = VehicleState {
+        x:0.0,
+        dx:0.0,
+        y:0.0,
+        dy:0.0,
         z:0.0,
-        dz:0.0
+        dz:0.0,
+        phi:0.0,
+        dphi:0.0,
+        theta:0.0,
+        dtheta:0.0,
+        psi:0.0,
+        dpsi:0.0
     };
 
     let _demands = Demands {
