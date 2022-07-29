5162bbd412ea2943c6255058da307c37b7c3a98f
simondlevy <simon.d.levy@gmail.com>
2022-07-29 00:06:29 -0400

    =More Rust work

--- Extras/rust/src/main.rs
+++ Extras/rust/src/main.rs
@@ -1,3 +1,4 @@
+use datatypes::datatypes::Demands;
 use datatypes::datatypes::VehicleState;
 
 pub mod datatypes;
@@ -7,6 +8,14 @@ fn main() {
     let _vehicle_state = VehicleState {
         z:0.0,
         dz:0.0
+    };
+
+    let _demands = Demands {
+
+        throttle:0.0,
+        roll:0.0,
+        pitch:0.0,
+        yaw:0.0
     };
 
     loop {
