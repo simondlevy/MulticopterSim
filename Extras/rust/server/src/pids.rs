0a69b2281afb378cad0cea1aff13760a7b360114
simondlevy <simon.d.levy@gmail.com>
2022-07-31 19:48:44 -0400

    Begin pids module

---
+++ Extras/rust/server/src/pids.rs
@@ -0,0 +1,1 @@
+pub mod yaw;
---
+++ Extras/rust/server/src/pids/yaw.rs
@@ -0,0 +1,6 @@
+// As with the module in auxiliary_building.rs our
+// function is private by default and made visible
+// through a preceeded "pub"
+pub fn foo() {
+
+}
