8f12e0ea0dd7732e0c751f8e605fd0e99c42b750
simondlevy <simon.d.levy@gmail.com>
2022-07-30 22:24:42 -0400

    =More Rust PID controller work

--- Extras/rust/pids/src/main.rs
+++ Extras/rust/pids/src/main.rs
@@ -17,34 +17,31 @@ struct Demands {
 
 enum PidController {
 
-    Altitude{r:f32},
+    Altitude{error_integral:f32},
 
     Angle{s:f32}
 }
 
-fn _run_alt_hold(
-    _throttle:f32,
-    _altitude:f32,
-    _climbrate:f32) -> (f32, f32, f32, f32) {
+fn run_alt_hold(alt_pid:PidController, _demands:&Demands, _vstate:&VehicleState)
+    ->  (Demands, PidController) {
 
-    (0.0, 0.0, 0.0, 0.0)
+    (Demands {
+        throttle:0.0,
+        roll:0.0,
+        pitch:0.0,
+        yaw:0.0}, 
+        alt_pid)
 }
 
 fn update(
     pid:PidController,
     demands:&Demands,
-    _vstate:&VehicleState) -> (Demands, PidController) {
+    vstate:&VehicleState) -> (Demands, PidController) {
 
     match pid {
 
-        PidController::Altitude{r} =>
-            (Demands {
-                throttle:demands.throttle, 
-                roll:demands.roll, 
-                pitch:demands.pitch, 
-                yaw:demands.yaw 
-            },
-            PidController::Altitude{r:r}),
+        PidController::Altitude{error_integral: _} =>
+            run_alt_hold(pid, demands, vstate),
 
         PidController::Angle{s} =>
             (Demands {
@@ -74,7 +71,7 @@ fn main() {
         //dpsi:0.0
     };
 
-    let alt_pid = PidController::Altitude{r:4.0};
+    let alt_pid = PidController::Altitude{error_integral:0.0};
     let angle_pid = PidController::Angle{s:2.0};
 
     let (_x, _new_alt_pid) = update(alt_pid, &demands, &vstate);
