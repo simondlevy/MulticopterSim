1a57f13253bd51d7d9068c79282240636cc676c9
simondlevy <simon.d.levy@gmail.com>
2022-08-03 15:27:36 -0400

    =More Rust PID work

--- Extras/rust/src/main.rs
+++ Extras/rust/src/main.rs
@@ -96,6 +96,8 @@ fn main() -> std::io::Result<()> {
 
         let demands = read_demands(in_buf);
 
+        println!("{}", demands.throttle);
+
         let (motors, new_pid_controller) =
             run_hackflight(demands, vehicle_state, pid_controller, &mixer::run);
 
