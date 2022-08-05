17f49b402a29336774ec2db45a60f1ddea508acb
simondlevy <simon.d.levy@gmail.com>
2022-08-04 21:33:08 -0400

    More C++ server work

--- Servers/cpp/hackflight.cpp
+++ Servers/cpp/hackflight.cpp
@@ -12,88 +12,29 @@
 
 #include "../../Source/SocketModule/sockets/UdpClientSocket.hpp"
 #include "../../Source/SocketModule/sockets/UdpServerSocket.hpp"
-//#include "../../Source/SocketModule/sockets/TcpClientSocket.hpp"
-
-//#include "../../Source/MainModule/dynamics/fixedpitch/QuadXBF.hpp"
 
 // Comms
 static const char * HOST = "127.0.0.1"; // localhost
 static uint16_t  MOTOR_PORT = 5000;
 static uint16_t  TELEM_PORT = 5001;
-//static uint16_t  IMAGE_PORT = 5002;
-
-// Image size
-//static uint16_t IMAGE_ROWS = 480;
-//static uint16_t IMAGE_COLS = 640;
 
 // Time constant
 static const double DELTA_T = 0.001;
 
-/*
-static Dynamics::vehicle_params_t vparams = {
-
-    // Estimated
-    2.E-06, // d torque constant [T=d*w^2]
-
-    // https://www.dji.com/phantom-4/info
-    1.380,  // m mass [kg]
-
-    // Estimated
-    2,      // Ix [kg*m^2] 
-    2,      // Iy [kg*m^2] 
-    3,      // Iz [kg*m^2] 
-    38E-04, // Jr prop inertial [kg*m^2] 
-
-    15000 // maxrpm
-};
-
-static FixedPitchDynamics::fixed_pitch_params_t fparams = {
-
-    // Estimated
-    5.E-06, // b force constatnt [F=b*w^2]
-    0.350   // l arm length [m]
-};
-*/
-
 int main(int argc, char ** argv)
 {
-    /*
-    // Allocate image bytes (rows * cols * rgba)
-    uint8_t image[IMAGE_ROWS * IMAGE_COLS * 4];
 
-    memset(image, 0, sizeof(image));
+    // Create sockets for telemetry in, motors out
+    UdpClientSocket motorClient = UdpClientSocket(HOST, MOTOR_PORT);
+    UdpServerSocket telemServer = UdpServerSocket(TELEM_PORT);
 
-    // Create proxy image with diagonal red stripe
-    for (uint16_t j=0; j<IMAGE_ROWS; ++j) {
-        for (uint16_t k=0; k<IMAGE_COLS; ++k) {
-            uint16_t l = (float)j/IMAGE_ROWS * IMAGE_COLS;
-            image[(j*IMAGE_COLS+l)*4] = 255;
-        }
-    }
-    */
 
+#if 0
     // Loop forever, waiting for clients
     while (true) {
 
-        // Create sockets for telemetry out, motors in
-        UdpClientSocket telemClient =
-            UdpClientSocket(HOST, TELEM_PORT);
-        UdpServerSocket motorServer =
-            UdpServerSocket(MOTOR_PORT);
-
-        // Create one-way server for images out
-        //TcpClientSocket imageSocket = TcpClientSocket(HOST, IMAGE_PORT);
-
-        // Create quadcopter dynamics model
-        //QuadXBFDynamics dynamics = QuadXBFDynamics(vparams, fparams);
-
         // Set up initial conditions
         double time = 0;
-        //double rotation[3] = {0,0,0};
-        //dynamics.init(rotation);
-
-        // Open image socket's connection to host
-        //imageSocket.openConnection();
 
         // Loop forever, communicating with client
         while (true) {
@@ -131,18 +72,13 @@ int main(int argc, char ** argv)
                     motorvals[1],
                     motorvals[2],
                     motorvals[3],
-                    0.0/*dynamics.x(Dynamics::STATE_Z)*/);
-
-            // Update dynamics with motor values
-            //dynamics.update(motorvals, DELTA_T);
-
-            // Set AGL to arbitrary positive value to o avoid kinematic trick
-            //dynamics.setAgl(1);
+                    0.0); //dynamics.x(Dynamics::STATE_Z));
 
             time += DELTA_T;
         }
 
     } // while (true)
+#endif
 
     return 0;
 }
