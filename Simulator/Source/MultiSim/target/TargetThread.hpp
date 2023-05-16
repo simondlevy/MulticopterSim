64f8f8f8559a3fcf3958f4550ca92fcf6e1237da
simondlevy <simon.d.levy@gmail.com>
2023-05-16 13:07:52 -0400

    =More target work

--- Simulator/Source/MultiSim/target/TargetThread.hpp
+++ Simulator/Source/MultiSim/target/TargetThread.hpp
@@ -12,9 +12,23 @@
 
 #define WIN32_LEAN_AND_MEAN
 
+#include "../sockets/UdpServerSocket.hpp"
+
 #include "Runtime/Core/Public/HAL/Runnable.h"
 
 class FTargetThread : public FRunnable {
+
+    private:
+
+        FRunnableThread * _thread = NULL;
+
+        bool _running = false;
+
+        // Socket comms
+        UdpServerSocket * _kinematicsServer = NULL;
+
+        // Vehicle pawn
+        APawn * _pawn = NULL;
 
     public:
 
@@ -22,12 +36,21 @@ class FTargetThread : public FRunnable {
         FTargetThread(
                 APawn * pawn,
                 const char * host="127.0.0.1",
-                const short port=5000)
+                const short port=5003)
         {
+            _pawn = pawn;
+
+            _thread =
+                FRunnableThread::Create(
+                        this, TEXT("FThreadedManage"), 0, TPri_BelowNormal);
         }
 
         ~FTargetThread(void)
         {
+            // Close sockets
+            UdpServerSocket::free(_kinematicsServer);
+
+           delete _thread;
         }
 
         virtual bool Init() override
