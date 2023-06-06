9eec286d5ccf2545d69a8a0bc61a0fbea6b11c53
simondlevy <simon.d.levy@gmail.com>
2023-06-06 01:38:21 -0400

    clean.bat removes entire Binaries directory

--- Simulator/clean.bat
+++ Simulator/clean.bat
@@ -9,7 +9,4 @@
 
 del /f /q *.sln *.vs > NUL
 
-del /f /q Binaries\Win64\*.target > NUL
-del /f /q Binaries\Win64\UE4* > NUL
-
-rmdir /q /s .vs Build DerivedDataCache Intermediate Saved > NUL:q
+rmdir /q /s .vs Binaries Build DerivedDataCache Intermediate Saved > NUL:q
