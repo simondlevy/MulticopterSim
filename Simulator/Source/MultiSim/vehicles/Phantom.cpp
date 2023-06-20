c3bd60695878790ece946af385ee81b5d24b474b
simondlevy <simon.d.levy@gmail.com>
2023-06-20 04:22:52 -0400

    Cleanup

--- Simulator/Source/MultiSim/vehicles/Phantom.cpp
+++ Simulator/Source/MultiSim/vehicles/Phantom.cpp
@@ -11,7 +11,12 @@
 
 APhantom::APhantom()
 {
-    buildPhantom(this, vehicle);
+    vehicle.buildFull(this, FrameStatics.mesh.Get());
+
+    addProp(PropCCWStatics.mesh.Get(), +1, +1);
+    addProp(PropCCWStatics.mesh.Get(), -1, -1);
+    addProp(PropCWStatics.mesh.Get(), +1, -1);
+    addProp(PropCWStatics.mesh.Get(), -1, +1);
 
     // Un-comment for camera
     // vehicle.addCamera(&camera);
@@ -46,3 +51,10 @@ void APhantom::Tick(float DeltaSeconds)
 
     Super::Tick(DeltaSeconds);
 }
+
+void APhantom::addProp(UStaticMesh * mesh, int8_t dx, int8_t dy)
+{
+    vehicle.addRotor(mesh, dx*0.12, dy*0.12, 0.16);
+}    
+
+
